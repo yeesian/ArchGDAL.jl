d42d1eb53ed05074c5dfcfa3b1b1d98d071adea6
Yeesian Ng <ngyeesian@gmail.com>
2016-08-08 15:06:15 -0400

    support configuration options (#7)
    * support configuration options
    
    * allow configoptions when registering drivers
    
    
    Former-commit-id: 695249675377d7463cdc10b1028bb3abef0d6c46

--- src/utils.jl
+++ src/utils.jl
@@ -56,3 +56,101 @@ metadatadomainlist{T <: GDAL.GDALMajorObjectH}(obj::Ptr{T}) =
 metadata{T <: GDAL.GDALMajorObjectH}(obj::Ptr{T}; domain::AbstractString="") =
     unsafe_loadstringlist(ccall((:GDALGetMetadata,GDAL.libgdal),Ptr{Cstring},
                             (Ptr{GDAL.GDALMajorObjectH},Cstring),obj,domain))
+
+"""
+Set a configuration option for GDAL/OGR use.
+
+Those options are defined as a (key, value) couple. The value corresponding to a
+key can be got later with the `getconfigoption()` method.
+
+### Parameters
+* `option`  the key of the option
+* `value`   the value of the option, or NULL to clear a setting.
+
+This mechanism is similar to environment variables, but options set with 
+`setconfigoption()` overrides, for `getconfigoption()` point of view, values 
+defined in the environment.
+
+If `setconfigoption()` is called several times with the same key, the value 
+provided during the last call will be used.
+"""
+setconfigoption(option::AbstractString, value::AbstractString) =
+    ccall((:CPLSetConfigOption,GDAL.libgdal),Void,(Cstring,Cstring),option,
+          value)
+
+"""
+This function can be used to clear a setting.
+
+Note: it will not unset an existing environment variable; it will 
+just unset a value previously set by `setconfigoption()`.
+"""
+clearconfigoption(option::AbstractString) =
+    ccall((:CPLSetConfigOption,GDAL.libgdal),Void,(Cstring,Ptr{UInt8}),option,
+          C_NULL)
+
+"""
+Get the value of a configuration option.
+
+The value is the value of a (key, value) option set with `setconfigoption()`.
+If the given option was not defined with `setconfigoption()`, it tries to find
+it in environment variables.
+
+### Parameters
+* `option`  the key of the option to retrieve
+* `default` a default value if the key does not match existing defined options
+
+### Returns
+the value associated to the key, or the default value if not found.
+"""
+function getconfigoption(option::AbstractString, default::AbstractString)
+    result = ccall((:CPLGetConfigOption,GDAL.libgdal),Ptr{UInt8},(Cstring,
+                   Cstring),option,default)
+    return (result == C_NULL) ? bytestring() : bytestring(result)
+end
+
+function getconfigoption(option::AbstractString)
+    result = ccall((:CPLGetConfigOption,GDAL.libgdal),Ptr{UInt8},(Cstring,
+                   Ptr{UInt8}),option,C_NULL)
+    return (result == C_NULL) ? bytestring() : bytestring(result)
+end
+
+"""
+Set a configuration option for GDAL/OGR use.
+
+Those options are defined as a (key, value) couple. The value corresponding to a
+key can be got later with the `getconfigoption()` method.
+
+### Parameters
+* `option`  the key of the option
+* `value`   the value of the option
+
+This function sets the configuration option that only applies in the current
+thread, as opposed to `setconfigoption()` which sets an option that applies on
+all threads.
+"""
+setthreadconfigoption(option::AbstractString, value::AbstractString) =
+    ccall((:CPLSetThreadLocalConfigOption,GDAL.libgdal),Void,(Cstring,Cstring),
+          option,value)
+
+"""
+This function can be used to clear a setting.
+
+Note: it will not unset an existing environment variable; it will 
+just unset a value previously set by `setthreadconfigoption()`.
+"""
+clearthreadconfigoption(option::AbstractString) =
+    ccall((:CPLSetThreadLocalConfigOption,GDAL.libgdal),Void,(Cstring,
+        Ptr{UInt8}),option,C_NULL)
+
+"Same as `getconfigoption()` but with settings from `setthreadconfigoption()`."
+function getthreadconfigoption(option::AbstractString, default::AbstractString)
+    result = ccall((:CPLGetThreadLocalConfigOption,GDAL.libgdal),Ptr{UInt8},
+                   (Cstring,Cstring),option,default)
+    return (result == C_NULL) ? bytestring() : bytestring(result)
+end
+
+function getthreadconfigoption(option::AbstractString)
+    result = ccall((:CPLGetThreadLocalConfigOption,GDAL.libgdal),Ptr{UInt8},
+                   (Cstring,Ptr{UInt8}),option,C_NULL)
+    return (result == C_NULL) ? bytestring() : bytestring(result)
+end
--- test/test_drivers.jl
+++ test/test_drivers.jl
@@ -1,6 +1,45 @@
 using FactCheck
 import ArchGDAL; const AG = ArchGDAL
 
+facts("Testing ConfigOptions") do
+    @fact AG.getconfigoption("GDAL_CACHEMAX") --> ""
+    AG.setconfigoption("GDAL_CACHEMAX", "64")
+    @fact AG.getconfigoption("GDAL_CACHEMAX") --> "64"
+    AG.clearconfigoption("GDAL_CACHEMAX")
+    @fact AG.getconfigoption("GDAL_CACHEMAX", "128") --> "128"
+
+    @fact AG.getthreadconfigoption("GDAL_CACHEMAX") --> ""
+    AG.setthreadconfigoption("GDAL_CACHEMAX","32")
+    @fact AG.getthreadconfigoption("GDAL_CACHEMAX") --> "32"
+    AG.clearthreadconfigoption("GDAL_CACHEMAX")
+    @fact AG.getthreadconfigoption("GDAL_CACHEMAX", "128") --> "128"
+
+    @fact AG.getconfigoption("GDAL_CACHEMAX") --> ""
+    @fact AG.getconfigoption("CPL_LOG_ERRORS") --> ""
+    @fact AG.getthreadconfigoption("GDAL_CACHEMAX") --> ""
+    @fact AG.getthreadconfigoption("CPL_LOG_ERRORS") --> ""
+
+    AG.registerdrivers(globalconfig=[("GDAL_CACHEMAX","64"),
+                                     ("CPL_LOG_ERRORS","ON")],
+                       threadconfig=[("GDAL_CACHEMAX","32"),
+                                     ("CPL_LOG_ERRORS","OFF")]) do
+        # it seems that thread settings overwrites global settings?
+        @fact AG.getconfigoption("GDAL_CACHEMAX") --> "32"
+        @fact AG.getconfigoption("CPL_LOG_ERRORS") --> "OFF"
+        @fact AG.getthreadconfigoption("GDAL_CACHEMAX") --> "32"
+        @fact AG.getthreadconfigoption("CPL_LOG_ERRORS") --> "OFF"
+    end
+
+    AG.registerdrivers(globalconfig=[("GDAL_CACHEMAX","64"),
+                                     ("CPL_LOG_ERRORS","ON")]) do
+        # everything normal here
+        @fact AG.getconfigoption("GDAL_CACHEMAX") --> "64"
+        @fact AG.getconfigoption("CPL_LOG_ERRORS") --> "ON"
+        @fact AG.getthreadconfigoption("GDAL_CACHEMAX") --> ""
+        @fact AG.getthreadconfigoption("CPL_LOG_ERRORS") --> ""
+    end
+end
+
 AG.registerdrivers() do
     drivers = AG.drivers()
     println(drivers)
@@ -13,8 +52,3 @@ AG.registerdrivers() do
         println("isvalid: $(AG.validate(AG.getdriver("GTiff"), options)) for $options")
     end
 end
-
-# Untested
-# destroy(drv::Driver) = GDAL.destroydriver(drv)
-# register(drv::Driver) = GDAL.registerdriver(drv)
-# deregister(drv::Driver) = GDAL.deregisterdriver(drv)
