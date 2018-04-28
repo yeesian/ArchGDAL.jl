e0a5748a6387a48e378ee720a2f8a0a3ca45e9e0
Yeesian Ng <ngyeesian@gmail.com>
2018-04-28 18:13:36 -0400

    add tests

---
+++ test/test_datastreams.jl
@@ -0,0 +1,21 @@
+using Base.Test
+import ArchGDAL; const AG = ArchGDAL
+import DataStreams
+
+@testset "DataStream Support" begin
+    df = AG.registerdrivers() do
+        AG.read("data/point.geojson") do dataset
+            DataStreams.Data.close!(DataStreams.Data.stream!(
+                AG.Source(AG.getlayer(dataset,0)), DataStreams.Data.Table
+            ))
+        end
+    end
+    @test df.FID == [2.0, 3.0, 0.0, 3.0]
+    @test df.pointname == ["point-a", "point-b", "a", "b"]
+    @test AG.toWKT.(df.geometry0) == [
+        "POINT (100 0)",
+        "POINT (100.2785 0.0893)",
+        "POINT (100 0)",
+        "POINT (100.2785 0.0893)"
+    ]
+end
