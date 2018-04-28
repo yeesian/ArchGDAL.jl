8f804dc45fedbbc7141c11a412e09afeec5504ff
Yeesian Ng <ngyeesian@gmail.com>
2018-04-28 17:20:00 -0400

    update for datastreams

--- src/datastreams.jl
+++ src/datastreams.jl
@@ -18,41 +18,17 @@ function Source(layer::FeatureLayer)
         [_FIELDTYPE[gettype(getfielddefn(layerdefn,i-1))] for i in 1:nfld]
     ]
     ArchGDAL.Source(
-        Data.Schema(header, types, nfeature(layer)),
+        Data.Schema(types, header, nfeature(layer)),
         layer,
         unsafe_nextfeature(layer),
         ngeom
     )
 end
+Data.schema(source::ArchGDAL.Source) = source.schema
 Data.isdone(s::ArchGDAL.Source, row, col) = s.feature.ptr == C_NULL
-Data.schema(source::ArchGDAL.Source, ::Type{Data.Field}) = source.schema
 Data.streamtype(::Type{ArchGDAL.Source}, ::Type{Data.Field}) = true
-
-function Data.streamfrom{T}(
-        source::ArchGDAL.Source,
-        ::Type{Data.Field},
-        ::Type{Nullable{T}},
-        row,
-        col
-    )
-    val = if col <= source.ngeom
-        Nullable{T}(getgeomfield(source.feature, col-1))
-    else
-        Nullable{T}(getfield(source.feature, col-source.ngeom-1))
-    end
-    if col == source.schema.cols
-        destroy(source.feature)
-        source.feature.ptr = @gdal(
-            OGR_L_GetNextFeature::GDALFeature,
-            source.featurelayer.ptr::GDALFeatureLayer
-        )
-        if row == source.schema.rows
-            @assert source.feature.ptr == C_NULL
-            resetreading!(source.featurelayer)
-        end
-    end
-    val
-end
+Data.accesspattern(source::ArchGDAL.Source) = Data.Sequential
+Data.reset!(source::ArchGDAL.Source) = resetreading!(source.featurelayer)
 
 function Data.streamfrom{T}(
         source::ArchGDAL.Source,
