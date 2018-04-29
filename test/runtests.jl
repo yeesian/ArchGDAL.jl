3ce118063f6bf19737d50084aed5c174100fd901
Yeesian Ng <ngyeesian@gmail.com>
2018-04-28 21:58:05 -0400

    Update for Datastreams (#38)
    * update for datastreams
    
    * add tests
    
    * disable codecov
    
    since we already have coveralls
    
    * enable tests for linux

--- test/runtests.jl
+++ test/runtests.jl
@@ -63,6 +63,7 @@ end
 
 cd(dirname(@__FILE__)) do
     isdir("tmp") || mkpath("tmp")
+    include("test_datastreams.jl")
     include("test_gdal_tutorials.jl")
     include("test_geometry.jl")
     include("test_types.jl")
