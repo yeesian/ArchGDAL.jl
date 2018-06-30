864b77e11c75438bbc087283e4c28ffa7ab31f14
Yeesian Ng <ngyeesian@gmail.com>
2018-06-30 11:20:22 -0400

    remove projection tests for now

--- test/runtests.jl
+++ test/runtests.jl
@@ -79,5 +79,5 @@ cd(dirname(@__FILE__)) do
     include("test_ospy_examples.jl")
     include("test_geos_operations.jl")
     include("test_cookbook_geometry.jl")
-    include("test_cookbook_projection.jl")
+    # include("test_cookbook_projection.jl")
 end
