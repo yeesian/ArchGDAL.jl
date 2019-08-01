776e3bdaa0d3979a54693144183988550d5bbd26
Yeesian Ng <ngyeesian@gmail.com>
2019-07-31 20:30:37 -0400

    [tests] add tests for methods of GDAL vector datasets

--- test/test_dataset.jl
+++ test/test_dataset.jl
@@ -2,7 +2,7 @@ using Test
 import GDAL
 import ArchGDAL; const AG = ArchGDAL
 
-@testset "Test methods for dataset" begin
+@testset "Test methods for raster dataset" begin
     AG.read("data/utmsmall.tif") do dataset
         AG.createcopy(dataset, filename = "/vsimem/utmcopy.tif") do copydataset
             @test AG.ngcp(copydataset) == 0
@@ -21,4 +21,39 @@ import ArchGDAL; const AG = ArchGDAL
     end
 end
 
-# untested: AG.deletelayer!(copydataset, 0)
+@testset "Test methods for vector dataset" begin
+    dataset1 = AG.read("data/point.geojson")
+    @test AG.nlayer(dataset1) == 1
+    layer1 = AG.getlayer(dataset1, 0)
+    @test AG.nfeature(layer1) == 4
+    
+    dataset2 = AG.createcopy(dataset1)
+    @test AG.nlayer(dataset2) == 1
+    @test AG.nfeature(AG.getlayer(dataset2, 0)) == -1 # it is not populated
+
+    dataset3 = AG.create(AG.getdriver("Memory"))
+    @test AG.nlayer(dataset3) == 0
+    
+    AG.copylayer(dataset3, layer1, "copy")
+    @test AG.nlayer(dataset3) == 1
+    layer3a = AG.getlayer(dataset3, "copy")
+    @test AG.nfeature(layer3a) == 4
+    @test AG.getname(layer3a) == "copy"
+    
+    AG.copylayer(dataset3, layer3a, "copy2")
+    @test AG.nlayer(dataset3) == 2
+    @test AG.getname(AG.getlayer(dataset3, 1)) == "copy2"
+    AG.deletelayer!(dataset3, 1)
+    @test AG.nlayer(dataset3) == 1
+
+    dataset4 = AG.create(tempname(), driver = AG.getdriver("KML"))
+    @test AG.nlayer(dataset4) == 0
+    layer4 = AG.createlayer(dataset4, "layer4", geom = GDAL.wkbLineString)
+    @test AG.nlayer(dataset4) == 1
+    
+    AG.create(tempname(), driver = AG.getdriver(dataset4)) do dataset5
+        @test AG.nlayer(dataset5) == 0
+        AG.createlayer(dataset5, "layer5", geom = GDAL.wkbLineString)
+        @test AG.nlayer(dataset5) == 1
+    end
+end
