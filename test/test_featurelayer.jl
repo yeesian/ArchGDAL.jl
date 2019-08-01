9d9f16470a395a93fd0a40cb0fead7b08bf4b76f
Yeesian Ng <ngyeesian@gmail.com>
2019-07-31 22:15:59 -0400

    [breaking] layer will now be reset at the start
    * this is different from starting in the middle when there is setnextbyindex!(layer, i). the user will have to do their own manual iteration if they wish for it.
    * added and updated some tests

--- test/test_featurelayer.jl
+++ test/test_featurelayer.jl
@@ -6,8 +6,6 @@ import ArchGDAL; const AG = ArchGDAL
     AG.read("data/point.geojson") do dataset
         AG.createcopy(dataset) do tmpcopy
             @test AG.nlayer(tmpcopy) == 1
-            # AG.deletelayer!(tmpcopy, 0)
-            # @test AG.nlayer(tmpcopy) == 0
             tmplayer = AG.getlayer(tmpcopy,0)
             @test AG.isignored(AG.getgeomfielddefn(AG.getlayerdefn(tmplayer),0)) == false
             AG.setignoredfields!(tmplayer, ["OGR_GEOMETRY"])
@@ -17,6 +15,9 @@ import ArchGDAL; const AG = ArchGDAL
 
         layer = AG.getlayer(dataset, 0)
         @test sprint(print, AG.getspatialref(layer)) == "Spatial Reference System: +proj=longlat +datum=WGS84 +no_defs "
+        AG.getspatialref(layer) do spref
+            @test sprint(print, spref) == "Spatial Reference System: +proj=longlat +datum=WGS84 +no_defs "
+        end
         @test AG.getfidcolname(layer) == ""
         @test AG.getgeomcolname(layer) == ""
         @test AG.nreference(layer) == 0
@@ -24,8 +25,23 @@ import ArchGDAL; const AG = ArchGDAL
         @test AG.nreference(layer) == 1
         AG.dereference(layer)
         @test AG.nreference(layer) == 0
+
+        @test AG.nfeature(layer) == 4
+        @test AG.getfield.(layer, 1) == ["point-a", "point-b", "a", "b"]
         AG.setspatialfilter!(layer,100,-1,100.1,1)
         @test AG.toWKT(AG.getspatialfilter(layer)) == "POLYGON ((100 -1,100 1,100.1 1.0,100.1 -1.0,100 -1))"
+        @test AG.nfeature(layer) == -1
+        @test AG.getfield.(layer, 1) == ["point-a", "a"]
+        AG.clearspatialfilter!(layer)
+
+        @test AG.nfeature(layer) == 4
+        @test AG.getfield.(layer, 1) == ["point-a", "point-b", "a", "b"]
+        @test AG.getgeomfieldindex(AG.getlayerdefn(layer)) == 0
+        AG.setspatialfilter!(layer,0,100,-1,100.1,1)
+        @test AG.toWKT(AG.getspatialfilter(layer)) == "POLYGON ((100 -1,100 1,100.1 1.0,100.1 -1.0,100 -1))"
+        @test AG.nfeature(layer) == -1
+        @test AG.getfield.(layer, 1) == ["point-a", "a"]
+
         AG.clone(AG.getspatialfilter(layer)) do poly
             n = 0; for feature in layer; n += 1 end; @test n == 2
             AG.clearspatialfilter!(layer)
@@ -44,8 +60,13 @@ import ArchGDAL; const AG = ArchGDAL
                 n = 0; for feature in layer; n += 1 end; @test n == 2
                 AG.clearattributefilter!(layer)
                 n = 0; for feature in layer; n += 1 end; @test n == 4
+                AG.nextfeature(layer) do feature
+                    @test AG.getfield(feature, 1) == "point-a"
+                end
                 AG.setnextbyindex!(layer, 2)
-                n = 0; for feature in layer; n += 1 end; @test n == 2
+                AG.nextfeature(layer) do feature
+                    @test AG.getfield(feature, 1) == "a"
+                end
                 @test AG.testcapability(layer,"OLCRandomWrite") == false
             end
         end
@@ -63,14 +84,19 @@ import ArchGDAL; const AG = ArchGDAL
 end
 
 # Untested:
+
 # setfeature!(layer::FeatureLayer, feature::Feature) 
 # deletefeature!(layer::FeatureLayer, i::Integer)
-# creategeomfield!(layer::FeatureLayer, field::GeomFieldDefn, approx::Bool = false)
+# writegeomfield!(layer::FeatureLayer, field::GeomFieldDefn, approx::Bool = false)
 # deletefield!(layer::FeatureLayer, i::Integer)
 # reorderfields!(layer::FeatureLayer, indices::Vector{Cint})
 # reorderfield!(layer::FeatureLayer, oldpos::Integer, newpos::Integer)
 # alterfielddefn!(layer::FeatureLayer, i::Integer, newfielddefn::FieldDefn, flags::UInt8)
-# setignoredfields!(layer::FeatureLayer, fieldnames)
+
+# starttransaction(layer)
+# committransaction(layer)
+# rollbacktransaction(layer)
+
 # intersection(input::FeatureLayer, method::FeatureLayer, result::FeatureLayer; options = StringList(C_NULL), progressfunc::Function = GDAL.C.GDALDummyProgress, progressdata = C_NULL)
 # union(input::FeatureLayer, method::FeatureLayer, result::FeatureLayer; options = StringList(C_NULL), progressdata = C_NULL, progressfunc::Function = GDAL.C.GDALDummyProgress)
 # symdifference(input::FeatureLayer, method::FeatureLayer, result::FeatureLayer; options = StringList(C_NULL), progressfunc::Function = GDAL.C.GDALDummyProgress, progressdata = C_NULL)
