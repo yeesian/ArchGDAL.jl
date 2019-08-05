4ef34d28f4f7220fbd91c5c12a127a7711f68399
Yeesian Ng <ngyeesian@gmail.com>
2019-08-05 00:22:41 -0400

    remove repeated calls to the same dataset

--- test/test_display.jl
+++ test/test_display.jl
@@ -17,18 +17,14 @@ end
         Number of feature layers: 1
           Layer 0: point (wkbPoint)
         """
-    end
 
-    read("data/point.geojson") do dataset
         @test sprint(print, AG.getlayer(dataset, 0)) == """
         Layer: point
           Geometry 0 (): [wkbPoint], POINT (100 0), POINT (100.2785 0.0893), ...
              Field 0 (FID): [OFTReal], 2.0, 3.0, 0.0, 3.0
              Field 1 (pointname): [OFTString], point-a, point-b, a, b
         """
-    end
 
-    read("data/point.geojson") do dataset
         AG.getfeature(AG.getlayer(dataset, 0), 2) do feature
             @test sprint(print, feature) == """
             Feature
@@ -51,9 +47,7 @@ end
           [GA_ReadOnly] Band 2 (Green): 2048 x 1024 (UInt8)
           [GA_ReadOnly] Band 3 (Blue): 2048 x 1024 (UInt8)
         """
-    end
 
-    read("gdalworkshop/world.tif") do dataset
         @test sprint(print, AG.getband(dataset, 1)) == """
         [GA_ReadOnly] Band 1 (Red): 2048 x 1024 (UInt8)
             blocksize: 256×256, nodata: -1.0e10, units: 1.0px + 0.0
