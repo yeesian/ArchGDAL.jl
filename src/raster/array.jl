984a2ca5f8b7578e634aed322108da8f5b369582
Rafael Schouten <rafaelschouten@gmail.com>
2019-12-08 09:20:07 +1100

    add setindex and RasterBand

--- src/raster/array.jl
+++ src/raster/array.jl
@@ -1,38 +1,90 @@
-Base.size(dataset::Dataset) = begin
-    band = getband(dataset, 1)
-    width(band), height(band), nraster(dataset)
-end
-
 const AllowedXY = Union{Integer,Colon,AbstractRange}
 const AllowedBand = Union{Integer,Colon,AbstractArray}
 
-Base.getindex(dataset::Dataset, x::AllowedXY, y::AllowedXY, bands::AllowedBand) = begin
-    # Convert colons to ranges
-    x, y, bands = map(convertcolon, (x, y, bands), size(dataset))
-    buffer = Array{getdatatype(getband(dataset, 1)),3}(undef, length.((x, y, bands))...)
-    xoffset, yoffset = first(x) - 1, first(y) - 1
-    xsize, ysize = length(x), length(y)
+
+# RasterBand indexing
+
+Base.size(band::RasterBand) = width(band), height(band)
+
+Base.getindex(band::RasterBand, x::AllowedXY, y::AllowedXY) = begin
+    I = map(colon2range, (x, y), size(band))
+    buffer = Array{getdatatype(band),2}(undef, length.(I)...)
+    # Calculate `read!` args and read
+    xoffset, yoffset = first.(I) .- 1
+    xsize, ysize = length.(I)
+    read!(band, buffer, xoffset, yoffset, xsize, ysize)
+    # Drop dimensions of integer indices
+    dimsize = map(length, dropint(I...))
+    if dimsize == ()
+        return buffer[1]
+    else
+        return reshape(buffer, dimsize...)
+    end
+end
+
+Base.setindex!(band::RasterBand, value, x::AllowedXY, y::AllowedXY) = begin
+    I = map(colon2range, (x, y), size(band))
+    if value isa AbstractArray
+        if size(value) != length.(I)
+            throw(ArgumentError("size of value $(size(value)) does not match indices $I"))
+        end
+    else
+        value = reshape([value], 1, 1)
+    end
+    # Calculate `write!` args and write
+    xoffset, yoffset = first.(I) .- 1
+    xsize, ysize = length.(I)
+    write!(band, value, xoffset, yoffset, xsize, ysize)
+end
+
+
+# Dataset indexing
+
+Base.size(dataset::Dataset) = (size(getband(dataset, 1))..., nraster(dataset))
+
+Base.getindex(dataset::Dataset, x::AllowedXY, y::AllowedXY, bands::AllowedBand=1) = begin
+    I = x, y, bands = map(colon2range, (x, y, bands), size(dataset))
+    buffer = Array{getdatatype(getband(dataset, 1)),3}(undef, length.(I)...)
+    # Calculate `read!` args and read
+    xoffset, yoffset = first.((x, y)) .- 1
+    xsize, ysize = length.((x, y))
     indices = bandindices(bands)
     read!(dataset, buffer, indices, xoffset, yoffset, xsize, ysize)
     # Drop dimensions of integer indices
-    reshape(buffer, dropint(x, y, bands)...)
+    dimsize = map(length, dropint(I...))
+    if dimsize == ()
+        return buffer[1]
+    else
+        return reshape(buffer, dimsize...)
+    end
 end
 
-Base.getindex(dataset::Dataset, x::Int, y::Int, band::Int) = begin
-    indices = bandindices(band)
-    buffer = Array{getdatatype(getband(dataset, 1)),3}(undef, 1, 1, 1)
-    xsize = ysize = 1
-    read!(dataset, buffer, indices, x - 1, y - 1, xsize, ysize)
-    # Indexing with all ints returns a scalar
-    buffer[1]
+Base.setindex!(dataset::Dataset, value, x::AllowedXY, y::AllowedXY, bands::AllowedBand=1) = begin
+    # Convert colons to ranges
+    I = x, y, bands = map(colon2range, (x, y, bands), size(dataset))
+    if value isa AbstractArray
+        if size(value) != length.(I)
+            throw(ArgumentError("size of value $(size(value)) does not match indices $I"))
+        end
+    else
+        value = reshape([value], 1, 1, 1)
+    end
+    # Calculate `write!` args and write
+    xoffset, yoffset = first.((x, y)) .- 1
+    xsize, ysize = length.((x, y))
+    indices = bandindices(bands)
+    write!(dataset, value, indices, xoffset, yoffset, xsize, ysize)
 end
+
+
+# Index conversion utilities
 
 dropint(x::Integer, args...) = dropint(args...)
-dropint(x::AbstractRange, args...) = (length(x), dropint(args...)...)
+dropint(x::AbstractRange, args...) = (x, dropint(args...)...)
 dropint() = ()
 
-convertcolon(i::Colon, sze) = 1:sze
-convertcolon(i, sze) = i
+colon2range(i::Colon, sze) = 1:sze
+colon2range(i, sze) = i
 
-bandindices(band::Int) = Cint[band]
+bandindices(band::Integer) = Cint[band]
 bandindices(bands::AbstractArray) = Cint[b for b in bands]
--- test/test_array.jl
+++ test/test_array.jl
@@ -2,35 +2,84 @@ using Test
 import GDAL
 import ArchGDAL; const AG = ArchGDAL
 
-buffer = AG.registerdrivers() do
+AG.registerdrivers() do
     AG.read("ospy/data4/aster.img") do ds
-        @testset "dims dropped correctly" begin
-            buffer = ds[:, :, :]
-            @test typeof(buffer) <: Array{UInt8,3}
-            buffer = ds[:, :, 1]
-            @test typeof(buffer) <: Array{UInt8,2}
-            buffer = ds[:, 1, 1]
-            @test typeof(buffer) <: Array{UInt8,1}
-            buffer = ds[1, 1, 1]
-            @test typeof(buffer) <: UInt8
+        @testset "Dataset indexing" begin
+            @testset "dims dropped correctly" begin
+                @test typeof(ds[:, :, :]) <: Array{UInt8,3}
+                @test typeof(ds[:, :, 1]) <: Array{UInt8,2}
+                @test typeof(ds[:, 1, 1]) <: Array{UInt8,1}
+                @test typeof(ds[1, 1, 1]) <: UInt8
+                @test typeof(ds[:, :]) <: Array{UInt8,2}
+                @test typeof(ds[:, 1]) <: Array{UInt8,1}
+                @test typeof(ds[1, 1]) <: UInt8
+            end
+            @testset "range indexing" begin
+                buffer = ds[1:AG.width(ds), 1:AG.height(ds), 1:1]
+                @test typeof(buffer) <: Array{UInt8,3}
+                total = sum(buffer)
+                count = sum(buffer .> 0)
+                @test total / count ≈ 76.33891347095299
+                @test total / (AG.height(ds) * AG.width(ds)) ≈ 47.55674749653172
+            end
+            @testset "colon indexing" begin
+                buffer = ds[:, :, 1]
+                @test buffer == ds[:, :] 
+                total = sum(buffer)
+                count = sum(buffer .> 0)
+                @test total / count ≈ 76.33891347095299
+                @test total / (AG.height(ds) * AG.width(ds)) ≈ 47.55674749653172
+            end
+            @testset "int indexing" begin
+                @test ds[755, 2107, 1] == 0xff
+            end
         end
-        @testset "range indexing" begin
-            buffer = ds[1:AG.width(ds), 1:AG.height(ds), 1:1]
-            @test typeof(buffer) <: Array{UInt8,3}
-            total = sum(buffer)
-            count = sum(buffer .> 0)
-            @test total / count ≈ 76.33891347095299
-            @test total / (AG.height(ds) * AG.width(ds)) ≈ 47.55674749653172
+        @testset "RasterBand indexing" begin
+            band = AG.getband(ds, 1)
+            @testset "dims dropped correctly" begin
+                @test typeof(band[:, :]) <: Array{UInt8,2}
+                @test typeof(band[:, 1]) <: Array{UInt8,1}
+                @test typeof(band[1, 1]) <: UInt8
+            end
+            @testset "range indexing" begin
+                buffer = band[1:AG.width(band), 1:AG.height(band)]
+                @test typeof(buffer) <: Array{UInt8,2}
+                total = sum(buffer)
+                count = sum(buffer .> 0)
+                @test total / count ≈ 76.33891347095299
+                @test total / (AG.height(band) * AG.width(band)) ≈ 47.55674749653172
+            end
+            @testset "colon indexing" begin
+                buffer = band[:, :]
+                total = sum(buffer)
+                count = sum(buffer .> 0)
+                @test total / count ≈ 76.33891347095299
+                @test total / (AG.height(band) * AG.width(band)) ≈ 47.55674749653172
+            end
+            @testset "int indexing" begin
+                @test band[755, 2107] == 0xff
+            end
         end
-        @testset "colon indexing" begin
-            buffer = ds[:, :, 1]
-            total = sum(buffer)
-            count = sum(buffer .> 0)
-            @test total / count ≈ 76.33891347095299
-            @test total / (AG.height(ds) * AG.width(ds)) ≈ 47.55674749653172
-        end
-        @testset "int indexing" begin
+    end
+end
+
+cp("ospy/data4/aster.img", "ospy/data4/aster_write.img"; force=true)
+AG.registerdrivers() do
+    AG.read("ospy/data4/aster_write.img"; flags=AG.OF_Update) do ds
+        @testset "Dataset setindex" begin
             @test ds[755, 2107, 1] == 0xff
+            ds[755, 2107, 1] = 0x00
+            @test ds[755, 2107, 1] == 0x00
+            ds[755:755, 2107:2107, 1] = reshape([0xff], 1, 1, 1)
+            @test ds[755, 2107, 1] == 0xff
+        end
+        band = AG.getband(ds, 1)
+        @testset "RasterBand setindex" begin
+            @test band[755, 2107] == 0xff
+            band[755, 2107] = 0x00
+            @test band[755, 2107] == 0x00
+            band[755:755, 2107:2107] = reshape([0xff], 1, 1)
+            @test band[755, 2107] == 0xff
         end
     end
 end
