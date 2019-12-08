8c75ae040e7aa410301701ec97aa8b3b3ebd1321
Rafael Schouten <rafaelschouten@gmail.com>
2019-12-09 09:49:57 +1100

    local methods have underscores

--- src/raster/array.jl
+++ src/raster/array.jl
@@ -9,14 +9,14 @@ Base.firstindex(band::RasterBand, d) = 1
 Base.lastindex(band::RasterBand, d) = size(band)[d]
 
 Base.getindex(band::RasterBand, x::AllowedXY, y::AllowedXY) = begin
-    I = map(colon2range, (x, y), size(band))
+    I = map(_colon2range, (x, y), size(band))
     buffer = Array{getdatatype(band),2}(undef, length.(I)...)
     # Calculate `read!` args and read
     xoffset, yoffset = first.(I) .- 1
     xsize, ysize = length.(I)
     read!(band, buffer, xoffset, yoffset, xsize, ysize)
     # Drop dimensions of integer indices
-    dimsize = map(length, dropint(I...))
+    dimsize = map(length, _dropint(I...))
     if dimsize == ()
         return buffer[1]
     else
@@ -25,9 +25,9 @@ Base.getindex(band::RasterBand, x::AllowedXY, y::AllowedXY) = begin
 end
 
 Base.setindex!(band::RasterBand, value, x::AllowedXY, y::AllowedXY) = begin
-    I = map(colon2range, (x, y), size(band))
+    I = map(_colon2range, (x, y), size(band))
     if value isa AbstractArray
-        if size(value) != length.(dropint(I...))
+        if size(value) != length.(_dropint(I...))
             throw(ArgumentError("size of value $(size(value)) does not match indices $I"))
         end
         value = reshape(value, length.(I))
@@ -48,15 +48,15 @@ Base.firstindex(dataset::Dataset, d) = 1
 Base.lastindex(dataset::Dataset, d) = size(dataset)[d]
 
 Base.getindex(dataset::Dataset, x::AllowedXY, y::AllowedXY, bands::AllowedBand=1) = begin
-    I = x, y, bands = map(colon2range, (x, y, bands), size(dataset))
+    I = x, y, bands = map(_colon2range, (x, y, bands), size(dataset))
     buffer = Array{getdatatype(getband(dataset, 1)),3}(undef, length.(I)...)
     # Calculate `read!` args and read
     xoffset, yoffset = first.((x, y)) .- 1
     xsize, ysize = length.((x, y))
-    indices = bandindices(bands)
+    indices = _bandindices(bands)
     read!(dataset, buffer, indices, xoffset, yoffset, xsize, ysize)
     # Drop dimensions of integer indices
-    dimsize = map(length, dropint(I...))
+    dimsize = map(length, _dropint(I...))
     if dimsize == ()
         return buffer[1]
     else
@@ -65,9 +65,9 @@ Base.getindex(dataset::Dataset, x::AllowedXY, y::AllowedXY, bands::AllowedBand=1
 end
 
 Base.setindex!(dataset::Dataset, value, x::AllowedXY, y::AllowedXY, bands::AllowedBand=1) = begin
-    I = x, y, bands = map(colon2range, (x, y, bands), size(dataset))
+    I = x, y, bands = map(_colon2range, (x, y, bands), size(dataset))
     if value isa AbstractArray
-        if size(value) != length.(dropint(I...))
+        if size(value) != length.(_dropint(I...))
             throw(ArgumentError("size of value $(size(value)) does not match indices $I"))
         end
         value = reshape(value, length.(I))
@@ -77,18 +77,18 @@ Base.setindex!(dataset::Dataset, value, x::AllowedXY, y::AllowedXY, bands::Allow
     # Calculate `write!` args and write
     xoffset, yoffset = first.((x, y)) .- 1
     xsize, ysize = length.((x, y))
-    indices = bandindices(bands)
+    indices = _bandindices(bands)
     write!(dataset, value, indices, xoffset, yoffset, xsize, ysize)
 end
 
 # Index conversion utilities
 
-dropint(x::Integer, args...) = dropint(args...)
-dropint(x::AbstractRange, args...) = (x, dropint(args...)...)
-dropint() = ()
+_dropint(x::Integer, args...) = _dropint(args...)
+_dropint(x::AbstractRange, args...) = (x, _dropint(args...)...)
+_dropint() = ()
 
-colon2range(i::Colon, sze) = 1:sze
-colon2range(i, sze) = i
+_colon2range(i::Colon, sze) = 1:sze
+_colon2range(i, sze) = i
 
-bandindices(band::Integer) = Cint[band]
-bandindices(bands::AbstractArray) = Cint[b for b in bands]
+_bandindices(band::Integer) = Cint[band]
+_bandindices(bands::AbstractArray) = Cint[b for b in bands]
