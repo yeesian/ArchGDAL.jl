befa6324e7031e22b23eaacf04b6ac2416c53459
Rafael Schouten <rafaelschouten@gmail.com>
2020-02-15 18:51:56 +1100

    update convert

--- src/convert.jl
+++ src/convert.jl
@@ -1,22 +1,36 @@
 # This file contains friendly type-pyracy on `convert` for GeoFormatTypes.jl types
 
-# Geometry conversions. 
-#
-# Convert to Geometry, then to the target format.
-# The Geom trait is needed to separate out convert for CRS for WellKnownText
-# and GML, which may contain both. It is handled in GeoFormatTypes.
-Base.convert(target::Type{<:GFT.GeoFormat}, mode::GFT.Geom, source::GFT.GeoFormat) =
-    convert(target, convert(Geometry, source))
+"""
+    convert(::Type{<:AbstractGeometry}, mode::Geom source::GeoFormat)
 
-Base.convert(::Type{<:Geometry}, source::GFT.AbstractWellKnownText) = 
+Convert a GeoFromat object to Geometry, then to the target format.
+The Geom trait is needed to separate out convert for CRS for WellKnownText
+and GML, which may contain both. It is handled in GeoFormatTypes.
+"""
+Base.convert(target::Type{<:GFT.GeoFormat}, mode::Type{GFT.Geom}, source::GFT.GeoFormat) =
+    convert(target, convert(AbstractGeometry, source))
+Base.convert(target::Type{<:GFT.GeoFormat}, mode::GFT.Geom, source::GFT.GeoFormat) =
+    convert(target, convert(AbstractGeometry, source))
+
+"""
+    convert(::Type{<:AbstractGeometry}, source::GeoFormat)
+
+Convert any GeoFormat geometry data to an ArchGDAL Gemoetry type
+"""
+Base.convert(::Type{<:AbstractGeometry}, source::GFT.AbstractWellKnownText) = 
     fromWKT(GFT.val(source))
-Base.convert(::Type{<:Geometry}, source::GFT.WellKnownBinary) = 
+Base.convert(::Type{<:AbstractGeometry}, source::GFT.WellKnownBinary) = 
     fromWKB(GFT.val(source))
-Base.convert(::Type{<:Geometry}, source::GFT.GeoJSON) = 
+Base.convert(::Type{<:AbstractGeometry}, source::GFT.GeoJSON) = 
     fromJSON(GFT.val(source))
-Base.convert(::Type{<:Geometry}, source::GFT.GML) = 
+Base.convert(::Type{<:AbstractGeometry}, source::GFT.GML) = 
     fromGML(GFT.val(source))
 
+"""
+    convert(::Type{<:AbstractGeometry}, source::GeoFormat)
+
+Convert AbstractGeometry data to any gemoetry GeoFormat 
+"""
 Base.convert(::Type{<:GFT.AbstractWellKnownText}, source::AbstractGeometry) = 
     GFT.WellKnownText(GFT.Geom(), toWKT(source))
 Base.convert(::Type{<:GFT.WellKnownBinary}, source::AbstractGeometry) = 
@@ -29,7 +43,13 @@ Base.convert(::Type{<:GFT.KML}, source::AbstractGeometry) =
     GFT.KML(toKML(source))
 
 
-# CRS conversions
+"""
+    convert(target::Type{GeoFormat}, mode::CRS, source::GeoFormat)
+
+Convert any GeoFormat crs data to another another GeoFormat type.
+"""
+Base.convert(target::Type{<:GFT.GeoFormat}, mode::Type{GFT.CRS}, source::GFT.GeoFormat) =
+    unsafe_convertcrs(target, importCRS(source))
 Base.convert(target::Type{<:GFT.GeoFormat}, mode::GFT.CRS, source::GFT.GeoFormat) =
     unsafe_convertcrs(target, importCRS(source))
 
@@ -40,7 +60,7 @@ unsafe_convertcrs(::Type{<:GFT.ProjString}, crsref) =
 unsafe_convertcrs(::Type{<:GFT.WellKnownText}, crsref) = 
     GFT.WellKnownText(GFT.CRS(), toWKT(crsref))
 unsafe_convertcrs(::Type{<:GFT.ESRIWellKnownText}, crsref) =
-    GFT.ESRIWellKnownText(toWKT(morphtoESRI!(crsref)))
+    GFT.ESRIWellKnownText(GFT.CRS(), toWKT(morphtoESRI!(crsref)))
 unsafe_convertcrs(::Type{<:GFT.GML}, crsref) = 
     GFT.GML(toXML(crsref))
 
--- test/test_convert.jl
+++ test/test_convert.jl
@@ -1,3 +1,6 @@
+using Test
+import ArchGDAL; const AG = ArchGDAL
+import GeoFormatTypes; const GFT = GeoFormatTypes
 
 @testset "convert point format" begin
     point = AG.createpoint(100, 70)
@@ -18,36 +21,13 @@ end
 
 
 @testset "convert crs format" begin
-    @testset "PROJ4 Format" begin
-        proj4326 = "+proj=longlat +datum=WGS84 +no_defs"
-        proj26912 = "+proj=utm +zone=12 +datum=NAD83 +units=m +no_defs"
-        AG.importPROJ4(proj4326) do spatialref
-            spatialref2 = AG.importPROJ4(proj26912)
-            @test AG.toPROJ4(spatialref2) == proj26912
-            AG.importPROJ4!(spatialref2, AG.toPROJ4(spatialref))
-            @test AG.toPROJ4(spatialref2) == proj4326
-        end
-    end
-
-    @testset "WKT Format" begin
-        wkt4326 = "GEOGCS[\"WGS 84\",DATUM[\"WGS_1984\",SPHEROID[\"WGS 84\",6378137,298.257223563,AUTHORITY[\"EPSG\",\"7030\"]],AUTHORITY[\"EPSG\",\"6326\"]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",\"8901\"]],UNIT[\"degree\",0.0174532925199433,AUTHORITY[\"EPSG\",\"9122\"]],AXIS[\"Latitude\",NORTH],AXIS[\"Longitude\",EAST],AUTHORITY[\"EPSG\",\"4326\"]]"
-        wkt26912 = "PROJCS[\"NAD83 / UTM zone 12N\",GEOGCS[\"NAD83\",DATUM[\"North_American_Datum_1983\",SPHEROID[\"GRS 1980\",6378137,298.257222101,AUTHORITY[\"EPSG\",\"7019\"]],TOWGS84[0,0,0,0,0,0,0],AUTHORITY[\"EPSG\",\"6269\"]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",\"8901\"]],UNIT[\"degree\",0.0174532925199433,AUTHORITY[\"EPSG\",\"9122\"]],AUTHORITY[\"EPSG\",\"4269\"]],PROJECTION[\"Transverse_Mercator\"],PARAMETER[\"latitude_of_origin\",0],PARAMETER[\"central_meridian\",-111],PARAMETER[\"scale_factor\",0.9996],PARAMETER[\"false_easting\",500000],PARAMETER[\"false_northing\",0],UNIT[\"metre\",1,AUTHORITY[\"EPSG\",\"9001\"]],AXIS[\"Easting\",EAST],AXIS[\"Northing\",NORTH],AUTHORITY[\"EPSG\",\"26912\"]]"
-        AG.importWKT(wkt4326) do spatialref
-            spatialref2 = AG.importWKT(wkt26912)
-            @test AG.toWKT(spatialref2) == wkt26912
-            AG.importWKT!(spatialref2, AG.toWKT(spatialref))
-            @test AG.toWKT(spatialref2) == wkt4326
-        end
-    end
-
-    @testset "ESRI Format" begin
-        esri4326 = "GEOGCS[\"WGS 84\",DATUM[\"WGS_1984\",SPHEROID[\"WGS 84\",6378137,298.257223563,AUTHORITY[\"EPSG\",\"7030\"]],AUTHORITY[\"EPSG\",\"6326\"]],PRIMEM[\"Greenwich\",0],UNIT[\"Degree\",0.0174532925199433],AXIS[\"Longitude\",EAST],AXIS[\"Latitude\",NORTH]]"
-        esri26912 = "PROJCS[\"NAD83 / UTM zone 12N\",GEOGCS[\"NAD83\",DATUM[\"North_American_Datum_1983\",SPHEROID[\"GRS 1980\",6378137,298.257222101,AUTHORITY[\"EPSG\",\"7019\"]],AUTHORITY[\"EPSG\",\"6269\"]],PRIMEM[\"Greenwich\",0],UNIT[\"Degree\",0.0174532925199433]],PROJECTION[\"Transverse_Mercator\"],PARAMETER[\"latitude_of_origin\",0],PARAMETER[\"central_meridian\",-111],PARAMETER[\"scale_factor\",0.9996],PARAMETER[\"false_easting\",500000],PARAMETER[\"false_northing\",0],UNIT[\"metre\",1,AUTHORITY[\"EPSG\",\"9001\"]],AXIS[\"Easting\",EAST],AXIS[\"Northing\",NORTH]]"
-        AG.importESRI(esri4326) do spatialref
-            spatialref2 = AG.importESRI(esri26912)
-            @test AG.toWKT(spatialref2) == esri26912
-            AG.importESRI!(spatialref2, AG.toWKT(spatialref))
-            @test AG.toWKT(spatialref2) == esri4326
-        end
-    end
+    wkt4326 = GFT.WellKnownText(GFT.CRS, "GEOGCS[\"WGS 84\",DATUM[\"WGS_1984\",SPHEROID[\"WGS 84\",6378137,298.257223563,AUTHORITY[\"EPSG\",\"7030\"]],AUTHORITY[\"EPSG\",\"6326\"]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",\"8901\"]],UNIT[\"degree\",0.0174532925199433,AUTHORITY[\"EPSG\",\"9122\"]],AXIS[\"Latitude\",NORTH],AXIS[\"Longitude\",EAST],AUTHORITY[\"EPSG\",\"4326\"]]")
+    wkt26912 = GFT.WellKnownText(GFT.CRS, "PROJCS[\"NAD83 / UTM zone 12N\",GEOGCS[\"NAD83\",DATUM[\"North_American_Datum_1983\",SPHEROID[\"GRS 1980\",6378137,298.257222101,AUTHORITY[\"EPSG\",\"7019\"]],TOWGS84[0,0,0,0,0,0,0],AUTHORITY[\"EPSG\",\"6269\"]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",\"8901\"]],UNIT[\"degree\",0.0174532925199433,AUTHORITY[\"EPSG\",\"9122\"]],AUTHORITY[\"EPSG\",\"4269\"]],PROJECTION[\"Transverse_Mercator\"],PARAMETER[\"latitude_of_origin\",0],PARAMETER[\"central_meridian\",-111],PARAMETER[\"scale_factor\",0.9996],PARAMETER[\"false_easting\",500000],PARAMETER[\"false_northing\",0],UNIT[\"metre\",1,AUTHORITY[\"EPSG\",\"9001\"]],AXIS[\"Easting\",EAST],AXIS[\"Northing\",NORTH],AUTHORITY[\"EPSG\",\"26912\"]]")
+    proj4326 = GFT.ProjString("+proj=longlat +datum=WGS84 +no_defs")
+    proj26912 = GFT.ProjString("+proj=utm +zone=12 +datum=NAD83 +units=m +no_defs")
+    esri4326 = GFT.ESRIWellKnownText(GFT.CRS, "GEOGCS[\"WGS 84\",DATUM[\"WGS_1984\",SPHEROID[\"WGS 84\",6378137,298.257223563,AUTHORITY[\"EPSG\",\"7030\"]],AUTHORITY[\"EPSG\",\"6326\"]],PRIMEM[\"Greenwich\",0],UNIT[\"Degree\",0.0174532925199433],AXIS[\"Longitude\",EAST],AXIS[\"Latitude\",NORTH]]")
+    esri26912 = GFT.ESRIWellKnownText(GFT.CRS, "PROJCS[\"NAD83 / UTM zone 12N\",GEOGCS[\"NAD83\",DATUM[\"North_American_Datum_1983\",SPHEROID[\"GRS 1980\",6378137,298.257222101,AUTHORITY[\"EPSG\",\"7019\"]],AUTHORITY[\"EPSG\",\"6269\"]],PRIMEM[\"Greenwich\",0],UNIT[\"Degree\",0.0174532925199433]],PROJECTION[\"Transverse_Mercator\"],PARAMETER[\"latitude_of_origin\",0],PARAMETER[\"central_meridian\",-111],PARAMETER[\"scale_factor\",0.9996],PARAMETER[\"false_easting\",500000],PARAMETER[\"false_northing\",0],UNIT[\"metre\",1,AUTHORITY[\"EPSG\",\"9001\"]],AXIS[\"Easting\",EAST],AXIS[\"Northing\",NORTH]]")
+    convert(GFT.ProjString, GFT.CRS(), wkt26912)
+    convert(GFT.WellKnownText, GFT.CRS(), proj26912)
+    convert(GFT.ESRIWellKnownText, GFT.CRS(), proj26912)
 end
