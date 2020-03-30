e7b0edc9505802e266baeaf042901b3084e30b60
ARSH SHARMA <184517@nith.ac.in>
2020-03-30 09:05:06 +0530

    Setup GeoTable, reading and parsing files[broken]

--- src/Tables.jl
+++ src/Tables.jl
@@ -1,57 +1,62 @@
-# #Since tables doesn't define a common "Source" type
+struct GeoTable{AbstractVector} <: Tables.AbstractColumns 
+    names::Array
+    geometry :: AG.IGeometry[]
+end
 
-# mutable struct Source <: TableSource
-#     schema::Tables.Schema
-#     featurelayer::AbstractFeatureLayer
-#     feature::ArchGDAL.Feature
-#     ngeom::Int
-# end
 
-# function Source(layer::AbstractFeatureLayer)
-#     featuredefn = layerdefn(layer)
-#     ngeometries = ngeom(featuredefn)
-#     nfld = nfield(featuredefn)
-#     header = [
-#         ["geometry$(i-1)" for i in 1:ngeometries];
-#         [getname(getfielddefn(featuredefn,i-1)) for i in 1:nfld]
-#     ]
-#     types = [
-#         [IGeometry for i in 1:ngeometries];
-#         [_FIELDTYPE[gettype(getfielddefn(featuredefn,i-1))] for i in 1:nfld]
-#     ]
-#     ArchGDAL.Source(
-#         Data.Schema(types, header, nfeature(layer)),
-#         layer,
-#         unsafe_nextfeature(layer),
-#         ngeometries
-#     )
-# end
-# Data.schema(source::ArchGDAL.Source) = source.schema
-# Data.isdone(s::ArchGDAL.Source, row, col) = s.feature.ptr == C_NULL
-# Data.streamtype(::Type{ArchGDAL.Source}, ::Type{Data.Field}) = true
-# Data.accesspattern(source::ArchGDAL.Source) = Data.Sequential
-# Data.reset!(source::ArchGDAL.Source) = resetreading!(source.featurelayer)
+function geotable(dataset::ArchGDAL.IDataset)
+    layer = getlayer(dataset, 0)
+    featuredefn = layerdefn(layer)
+    ngeometries = ngeom(featuredefn)
+    nfld = nfield(featuredefn)
+    featuredefn = layerdefn(layer)
+    
+    d = Dict{String, Vector}()
+    featuredefn = AG.layerdefn(layer)
+    for field_no in 0:nfield-1
+        field = AG.getfielddefn(featuredefn, field_no)
+        name = AG.getname(field)
+        typ = AG._FIELDTYPE[AG.gettype(field)]
+        d[name] = typ[]
+    end
+    
+    d["geometry"] = AG.IGeometry[]
 
-# function Data.streamfrom(
-#         source::ArchGDAL.Source,
-#         ::Type{Data.Field},
-#         ::Type{T},
-#         row,
-#         col
-#     ) where T
-#     val = if col <= source.ngeom
-#         @assert T <: IGeometry
-#         getgeom(source.feature, col-1)
-#     else
-#         T(getfield(source.feature, col - source.ngeom - 1))
-#     end
-#     if col == source.schema.cols
-#         destroy(source.feature)
-#         source.feature.ptr = GDAL.ogr_l_getnextfeature(source.featurelayer.ptr)
-#         if row == source.schema.rows
-#             @assert source.feature.ptr == C_NULL
-#             resetreading!(source.featurelayer)
-#         end
-#     end
-#     val
-# end
+    for fid in 0:nfeat-1
+        AG.getfeature(layer, fid) do feature
+            for (k, v) in pairs(d)
+                if k == "geometry"
+                    val = AG.getgeom(feature, 0)
+                else
+                    val = AG.getfield(feature, k)
+                end
+                push!(v, val)
+            end
+        end
+    end
+    
+
+    # names = [
+    #     ["geometry$(i-1)" for i in 1:ngeometries]; #in case there are more than one geometries
+    #     [AG.getname(AG.getfielddefn(featuredefn,i-1)) for i in 1:nfld]
+    # ]
+    
+    
+    # types = [
+    #     [AG.IGeometry for i in 1:ngeometries];
+    #     [AG._FIELDTYPE[AG.gettype(AG.getfielddefn(featuredefn,i-1))] for i in 1:nfld]
+    # ]
+    
+    #   val = if nfld <= ngeom
+    #             getgeom(feature, col-1)
+    #         else
+    #             T(getfield(feature, col - ngeom - 1))
+    #         end
+
+    Tables.istable(::Type{<:GeoTable}) = true
+    # getter methods to avoid getproperty clash
+    names(g::GeoTable) = getfield(g, :names)
+    
+    geometry(g::GeoTable) = getfield(g, :geometry)
+    # schema is column names and types
+    Tables.schema(g::GeoTable{T}) where {T} = Tables.Schema(names(g), )
