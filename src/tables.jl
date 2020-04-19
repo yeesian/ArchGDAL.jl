684d8acdfdf4269760e9cdb15f1ca532eb115840
ARSH SHARMA <184517@nith.ac.in>
2020-04-19 17:48:14 +0530

    Added tests, cleanup code

--- src/tables.jl
+++ src/tables.jl
@@ -27,28 +27,58 @@ function Tables.schema(layer::AG.AbstractFeatureLayer)
     fielddefns = (AG.getfielddefn(featuredefn, i) for i in 0:nfield-1)
     geomdefns = (ArchGDAL.getgeomdefn(featuredefn, i) for i in 0:ngeom-1)
 
-    names = Tuple(AG.getname(fielddefn) for fielddefn in fielddefns)
+    names_fields = Tuple(AG.getname(fielddefn) for fielddefn in fielddefns)
     geom_names = Tuple(ArchGDAL.getname(geomdefn) for geomdefn in geomdefns)
+    names = (names_fields..., geom_names...)
     
-    types = Tuple(AG._FIELDTYPE[AG.gettype(fielddefn)] for fielddefn in fielddefns)
+    types_fields = Tuple(AG._FIELDTYPE[AG.gettype(fielddefn)] for fielddefn in fielddefns)
     geom_types = Tuple(ArchGDAL.gettype(geomdefn) for geomdefn in geomdefns)
- 
+    types = (types_fields..., geom_types...)
+    
+    
+
     Tables.Schema(names, types)
 end
 
 function Base.iterate(gt::GeoTable, st = 0)
     layer = gt.layer
-    schema = Tables.schema(layer)
-    T = NamedTuple{schema.names, Tuple{schema.types...}}
     AG.resetreading!(layer)
     nfeat = AG.nfeature(layer)
     nfield = AG.nfield(layer)
-    Row = T[]
+    ngeom = AG.ngeom(layer)
+    featuredefn = layerdefn(layer)
+
+    d = Dict{String, Vector}()
+
+    for field_no in 0:nfield-1
+        field = getfielddefn(featuredefn, field_no)
+        name = getname(field)
+        typ = _FIELDTYPE[gettype(field)]
+        d[name] = typ[]
+    end
+    d["geometry"] = IGeometry[]
+    
     st >= nfeat && return nothing
     AG.nextfeature(layer) do feature
-        # AG.getgeom(feature, 0)  # TODO
-        push!(Row, T(AG.getfield(feature, j) for j in 0:nfield-1))
+        for (k, v) in pairs(d)
+            if k == "geometry"
+                val = getgeom(feature, 0)
+            else
+                val = getfield(feature, k)
+            end
+            push!(v, val)
         end
+    end
+    
+    keys_tup = ()
+    for _key in keys(d)
+        keys_tup = (keys_tup..., Symbol(_key))
+    end
+    vals_tup = Tuple(values(d))
+    
+    #Using the tables interface
+    Row = Tables.rowtable(NamedTuple{keys_tup}(vals_tup))
+    
     return FeatureRow(Row, st), st + 1
 end
 
@@ -57,92 +87,7 @@ Tables.istable(::Type{<:GeoTable}) = true
 Tables.rowaccess(::Type{<:GeoTable}) = true
 Tables.rows(gt::GeoTable) = gt
 
-Base.propertynames(f::FeatureRow) = (Tables.schema(layer)).names
-#type of geometry
-# geometry(fr::FeatureRow) = json(fr).geometry
-
 
 function Base.show(io::IO, gt::GeoTable)
     println(io, "GeoTable with $(ArchGDAL.nfeature(gt.layer)) Features")
 end
-# function Base.show(io::IO, fr::FeatureRow)
-#     println(io, "Feature with geometry type $(geometry(fr).type) and properties $(propertynames(fr))")
-# end
-# Base.show(io::IO, ::MIME"text/plain", gt::GeoTable) = show(io, gt)
-# Base.show(io::IO, ::MIME"text/plain", fr::FeatureRow) = show(io, fr)
-
-
-
-# Base.IteratorSize(::Type{<:GeoTable}) = Base.HasLength()
-# Base.length(gt::GeoTable) = length(layer(gt))
-# Base.IteratorEltype(::Type{<:GeoTable}) = Base.HasEltype()
-
-# # read only AbstractVector
-# Base.size(gt::GeoTable) = size(json(gt))
-# Base.getindex(gt::GeoTable, i) = Feature(json(gt)[i])
-# Base.IndexStyle(::Type{<:GeoTable}) = IndexLinear()
-
-# miss(x) = ifelse(x === nothing, missing, x)
-
-
-# "Access the properties JSON3.Object of a Feature"
-# properties(f::Feature) = json(f).properties
-# "Access the JSON3.Object that represents the Feature"
-# json(f::Feature) = getfield(f, :js    on)
-# "Access the JSON3.Array that represents the FeatureCollection"
-# json(f::FeatureCollection) = getfield(f, :json)
-
-# "Access the JSON3.Object that represents the Feature's geometry"
-
-# """
-# Get a specific property of the Feature
-# Returns missing for null/nothing or not present, to work nicely with
-# properties that are not defined for every feature. If it is a table,
-# it should in some sense be defined.
-# """
-# function Base.getproperty(f::Feature, nm::Symbol)
-#     props = properties(f)
-#     val = get(props, nm, missing)
-#     miss(val)
-# end
-
-# function geotable(layer::Union{IFeatureLayer, FeatureLayer}, i::Int) 
-#     featuredefn = layerdefn(layer)
-#     ngeometries = ngeom(featuredefn)
-#     nfield = ArchGDAL.nfield(featuredefn)
-#     featuredefn = layerdefn(layer)
-#     nfeat = nfeature(layer)
-
-#     d = Dict{String, Vector}()
-
-#     for field_no in 0:nfield-1
-#         field = getfielddefn(featuredefn, field_no)
-#         name = getname(field)
-#         typ = _FIELDTYPE[gettype(field)]
-#         d[name] = typ[]
-#     end
-    
-#     d["geometry"] = IGeometry[]
-    
-#     for fid in 0:nfeat-1
-#         getfeature(layer, fid) do feature
-#             for (k, v) in pairs(d)
-#                 if k == "geometry"
-#                     val = getgeom(feature, 0)
-#                 else
-#                     val = getfield(feature, k)
-#                 end
-#                 push!(v, val)
-#             end
-#         end
-#     end
-#     keys_tup = ()
-#     for _key in keys(d)
-#         keys_tup = (keys_tup..., Symbol(_key))
-#     end
-#     vals_tup = Tuple(values(d))
-    
-#     #Using the tables interface
-#     Rows = Tables.rowtable(NamedTuple{keys_tup}(vals_tup))
-#     return Rows
-# end
--- test/test_tables.jl
+++ test/test_tables.jl
@@ -2,63 +2,31 @@ using Test
 import ArchGDAL; const AG = ArchGDAL
 using Tables
 
-# @testset "DataStream Support" begin
-#     df = AG.read("data/point.geojson") do dataset
-#         DataStreams.Data.close!(DataStreams.Data.stream!(
-#             AG.Source(AG.getlayer(dataset,0)), DataStreams.Data.Table
-#         ))
-#     end
-#     @test df.FID == [2.0, 3.0, 0.0, 3.0]
-#     @test df.pointname == ["point-a", "point-b", "a", "b"]
-#     @test AG.toWKT.(df.geometry0) == [
-#         "POINT (100 0)",
-#         "POINT (100.2785 0.0893)",
-#         "POINT (100 0)",
-#         "POINT (100.2785 0.0893)"
-#     ]
-# end
+@testset "Tables Support" begin
+    dataset = AG.read(joinpath(@__DIR__, "data/point.geojson"))
+    layer = AG.getlayer(dataset, 0)
 
-dataset = AG.read(joinpath(@__DIR__, "data/point.geojson"))
-layer = AG.getlayer(dataset, 0)
-
-nfeat = AG.nfeature(layer)
-nfield = AG.nfield(layer)
-featuredefn = AG.layerdefn(layer)
-ngeometries = AG.ngeom(featuredefn)
-
-Tables.istable(layer::AG.AbstractFeatureLayer) = true
-Tables.rowaccess(layer::AG.AbstractFeatureLayer) = true
-
-function Tables.schema(layer::AG.AbstractFeatureLayer)
-    # TODO include names and types of geometry columns
-    featuredefn = AG.layerdefn(layer)
-    fielddefns = (AG.getfielddefn(featuredefn, i) for i in 0:nfield-1)
-    names = Tuple(AG.getname(fielddefn) for fielddefn in fielddefns)
-    types = Tuple(AG._FIELDTYPE[AG.gettype(fielddefn)] for fielddefn in fielddefns)
-    Tables.Schema(names, types)
-end
-
-schema = Tables.schema(layer)
-
-function Tables.rows(layer::AG.AbstractFeatureLayer)
-    # TODO return an iterator rather than a vector of NamedTuples
-    schema = Tables.schema(layer)
-    T = NamedTuple{schema.names, Tuple{schema.types...}}
-    AG.resetreading!(layer)
     nfeat = AG.nfeature(layer)
     nfield = AG.nfield(layer)
-    rows = T[]
-    for _ in 1:nfeat
-        AG.nextfeature(layer) do feature
-            # AG.getgeom(feature, 0)  # TODO
-            push!(rows, T(AG.getfield(feature, j) for j in 0:nfield-1))
-        end
-    end
-    return rows
+    featuredefn = AG.layerdefn(layer)
+    ngeometries = AG.ngeom(featuredefn)
+
+    #testing the main GeoTable function
+    @test sprint(print, AG.GeoTable(layer)) == "GeoTable with 7 Features\n"
+    @test sprint(print, AG.GeoTable(layer)) != "Layer is not a valid ArchGDAL layer\n"      
+    
+    #testing Tables.schema
+    @test sprint(print, AG.Tables.schema(layer)) == "Tables.Schema:\n :fid        Int32             \n :uuid       String            \n Symbol(\"\")  GDAL.wkbLineString"
+
+    #testing Base.iterate
+    @test sprint(print, AG.Base.iterate(AG.GeoTable(layer))) == "(ArchGDAL.FeatureRow(NamedTuple{(:uuid, :geometry, :fid),Tuple{String,ArchGDAL.IGeometry,Int32}}[(uuid = \"{2c66f90e-befb-49d5-8083-c5642194be06}\", geometry = Geometry: LINESTRING (-9.14617603432751 2.17229654185419,-9. ... 9273), fid = 183521)], 0), 1)"
+
+    @test Tables.istable(AG.GeoTable(layer)) == true
+    @test Tables.rowaccess(AG.GeoTable(layer)) == true
+    @test Tables.rows(AG.GeoTable(layer)) == AG.GeoTable(layer)
+
+    @test Base.size(AG.GeoTable(layer)) == 7
+    @test Base.length(AG.GeoTable(layer)) == 21
+
 end
 
-Tables.rows(layer)
-
-# TODO getcolumn support
-# using DataFrames
-# DataFrame(layer)
