f4aa595b402888d52382e9d0e659ee0475d6d119
ARSH SHARMA <184517@nith.ac.in>
2020-04-19 19:25:21 +0530

    Update tests for point.geojson

--- test/test_tables.jl
+++ test/test_tables.jl
@@ -12,21 +12,20 @@ using Tables
     ngeometries = AG.ngeom(featuredefn)
 
     #testing the main GeoTable function
-    @test sprint(print, AG.GeoTable(layer)) == "GeoTable with 7 Features\n"
+    @test sprint(print, AG.GeoTable(layer)) == "GeoTable with 4 Features\n"
     @test sprint(print, AG.GeoTable(layer)) != "Layer is not a valid ArchGDAL layer\n"      
     
     #testing Tables.schema
-    @test sprint(print, AG.Tables.schema(layer)) == "Tables.Schema:\n :fid        Int32             \n :uuid       String            \n Symbol(\"\")  GDAL.wkbLineString"
-
+    @test sprint(print, AG.Tables.schema(layer)) == "Tables.Schema:\n :FID        Float64      \n :pointname  String       \n Symbol(\"\")  GDAL.wkbPoint"
     #testing Base.iterate
-    @test sprint(print, AG.Base.iterate(AG.GeoTable(layer))) == "(ArchGDAL.FeatureRow(NamedTuple{(:uuid, :geometry, :fid),Tuple{String,ArchGDAL.IGeometry,Int32}}[(uuid = \"{2c66f90e-befb-49d5-8083-c5642194be06}\", geometry = Geometry: LINESTRING (-9.14617603432751 2.17229654185419,-9. ... 9273), fid = 183521)], 0), 1)"
-
+    @test sprint(print, AG.Base.iterate(AG.GeoTable(layer))) == "(ArchGDAL.FeatureRow(NamedTuple{(:pointname, :geometry, :FID),Tuple{String,ArchGDAL.IGeometry,Float64}}[(pointname = \"point-a\", geometry = Geometry: POINT (100 0), FID = 2.0)], 0), 1)"
     @test Tables.istable(AG.GeoTable(layer)) == true
     @test Tables.rowaccess(AG.GeoTable(layer)) == true
     @test Tables.rows(AG.GeoTable(layer)) == AG.GeoTable(layer)
 
-    @test Base.size(AG.GeoTable(layer)) == 7
-    @test Base.length(AG.GeoTable(layer)) == 21
+    @test Base.size(AG.GeoTable(layer)) == 4
+    @test Base.length(AG.GeoTable(layer)) == 12
 
 end
+
 
