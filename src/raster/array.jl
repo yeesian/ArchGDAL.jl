7d86bfacf4657175d369c483fdb1229c16665ab5
Fabian Gans <fgans@bgc-jena.mpg.de>
2020-04-20 18:37:08 +0200

    Add more doc strings and remove unnecessary comments

--- src/raster/array.jl
+++ src/raster/array.jl
@@ -4,12 +4,25 @@ const AllowedXY = Union{Integer,Colon,AbstractRange}
 const AllowedBand = Union{Integer,Colon,AbstractArray}
 
 
-# Define a RasterDataset type
+"""
+    struct RasterDataset{T} <: AbstractDiskArray{T,3}
+
+This data structure is returned by the `readraster` function and
+is a wrapper for a GDAL dataset. This wrapper is to signal the
+user that the dataset should be treated as a 3D AbstractArray
+where the first to dimensions correspond to latitude and longitude
+and the third dimension correpsonds to different raster bands.
+
+Constructing a RasterDataset will error if the raster bands do not
+have all the same size and a common element data type.
+"""
 struct RasterDataset{T,DS} <: AbstractDiskArray{T,3}
     ds::DS
     size::Tuple{Int,Int,Int}
 end
 #Forward a few functions
+#Here we try to include all functions that are relevant
+#for raster-like datasets.
 for f in (:getgeotransform, :nraster, :getband, :getproj,
   :width, :height, :destroy)
 
@@ -20,6 +33,12 @@ function RasterDataset(ds::AbstractDataset)
     s = _common_size(ds)
     RasterDataset{_dataset_type(ds), typeof(ds)}(ds,s)
 end
+"""
+  _dataset_type(ds::AbstractDataset)
+
+Tries to determine a common dataset type for all the bands
+in a raster dataset.
+"""
 function _dataset_type(ds::AbstractDataset)
   alldatatypes = map(1:nraster(ds)) do i
     b = getband(ds,i)
@@ -27,6 +46,12 @@ function _dataset_type(ds::AbstractDataset)
   end
   reduce(promote_type,alldatatypes)
 end
+"""
+    _common_size(ds::AbstractDataset)
+
+Determines the size of the raster bands in a dataset and errors
+if the sizes are not unique.
+"""
 function _common_size(ds::AbstractDataset)
   nr = nraster(ds)
   allsizes = map(1:nr) do i
@@ -39,14 +64,21 @@ function _common_size(ds::AbstractDataset)
 end
 getband(ds::RasterDataset,i) = getband(ds.ds,i)
 unsafe_readraster(args...;kwargs...)  = RasterDataset(unsafe_read(args...;kwargs...))
+"""
+    ArchGDAL.readraster(s::String;kwargs...)
+
+Opens a GDAL raster dataset. The difference to `ArchGDAL.read` is that
+this function returns a `RasterDataset`, which is a subtype of `AbstractDiskArray{T,3}`,
+so that users can operate on the array using direct indexing.
+"""
 readraster(s::String;kwargs...) = RasterDataset(read(s;kwargs...))
+
 function eachchunk(ds::RasterDataset)
   subchunks = eachchunk(getband(ds,1))
   GridChunks(ds,(subchunks.chunksize...,1))
 end
 DiskArrays.haschunks(::RasterDataset) = DiskArrays.Chunked()
 DiskArrays.haschunks(::AbstractRasterBand) = DiskArrays.Chunked()
-# AbstractRasterBand indexing
 
 Base.size(band::AbstractRasterBand) = width(band), height(band)
 # Don't know if we need these, commenting out for the moment
@@ -60,14 +92,12 @@ function eachchunk(band::AbstractRasterBand)
 end
 
 DiskArrays.readblock!(band::AbstractRasterBand, buffer, x::AbstractUnitRange, y::AbstractUnitRange) = begin
-    # Calculate `read!` args and read
     xoffset, yoffset = first(x)-1, first(y)-1
     xsize, ysize = length(x), length(y)
     read!(band, buffer, xoffset, yoffset, xsize, ysize)
 end
 
 DiskArrays.writeblock!(band::AbstractRasterBand, value, x::AbstractUnitRange, y::AbstractUnitRange) = begin
-    # Calculate `read!` args and read
     xoffset, yoffset = first(x)-1, first(y)-1
     xsize, ysize = length(x), length(y)
     write!(band, value, xoffset, yoffset, xsize, ysize)
@@ -87,7 +117,6 @@ end
 
 
 DiskArrays.readblock!(dataset::RasterDataset, buffer::Array, x::AbstractUnitRange, y::AbstractUnitRange, z::AbstractUnitRange) = begin
-    # Calculate `read!` args and read
     xoffset, yoffset = first.((x, y)) .- 1
     xsize, ysize= length.((x, y))
     indices  = [Cint(i) for i in z]
@@ -100,7 +129,5 @@ DiskArrays.writeblock!(dataset::RasterDataset, value, x::AbstractUnitRange, y::A
     indices  = [Cint(i) for i in bands]
     write!(dataset.ds, value, indices, xoffset, yoffset, xsize, ysize)
 end
-
-# Index conversion utilities
 
 Array(dataset::RasterDataset) = dataset[:,:,:]
