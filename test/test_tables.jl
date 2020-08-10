e6b0e9e47a2f4162fdec6b8a41a1bedc67dd8b4a
ARSH SHARMA <184517@nith.ac.in>
2020-08-10 12:49:09 +0530

    Update test

--- test/test_tables.jl
+++ test/test_tables.jl
@@ -5,7 +5,6 @@ using Tables
 @testset "Tables Support" begin
     dataset = AG.read(joinpath(@__DIR__, "data/point.geojson"))
     layer = AG.getlayer(dataset, 0)
-
     nfeat = AG.nfeature(layer)
     nfield = AG.nfield(layer)
     featuredefn = AG.layerdefn(layer)
@@ -13,17 +12,13 @@ using Tables
 
     @test sprint(print, AG.GeoTable(layer)) == "GeoTable with 4 Features\n"
     @test sprint(print, AG.GeoTable(layer)) != "Layer is not a valid ArchGDAL layer\n"      
-    
-    @test sprint(print, AG.Tables.schema(layer)) == "Tables.Schema:\n :FID        Float64      \n :pointname  String       \n Symbol(\"\")  GDAL.wkbPoint"
+    @test sprint(print, AG.Tables.schema(layer)) == "Tables.Schema:\n :FID        Float64      \n :pointname  String       \n Symbol(\"\")  wkbPoint::OGRwkbGeometryType = 0x00000001"
     @test sprint(print, AG.Base.iterate(AG.GeoTable(layer))) == "(ArchGDAL.FeatureRow(NamedTuple{(:pointname, :geometry, :FID),Tuple{String,ArchGDAL.IGeometry,Float64}}[(pointname = \"point-a\", geometry = Geometry: POINT (100 0), FID = 2.0)], 0), 1)"
-
     @test Tables.istable(AG.GeoTable(layer)) == true
     @test Tables.rowaccess(AG.GeoTable(layer)) == true
     @test Tables.rows(AG.GeoTable(layer)) == AG.GeoTable(layer)
-
     @test Base.size(AG.GeoTable(layer)) == 4
     @test Base.length(AG.GeoTable(layer)) == 12
-
 end
 
 
