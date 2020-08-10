23cc9f1af2f21afb3a06c598b3ffddac4736f8cc
ARSH SHARMA <184517@nith.ac.in>
2020-08-10 14:53:50 +0530

    Schema test for 1.5

--- test/test_tables.jl
+++ test/test_tables.jl
@@ -12,7 +12,7 @@ using Tables
 
     @test sprint(print, AG.GeoTable(layer)) == "GeoTable with 4 Features\n"
     @test sprint(print, AG.GeoTable(layer)) != "Layer is not a valid ArchGDAL layer\n"      
-    @test sprint(print, AG.Tables.schema(layer)) == "Tables.Schema:\n :FID        Float64      \n :pointname  String       \n Symbol(\"\")  wkbPoint::OGRwkbGeometryType = 0x00000001"
+    @test sprint(print, AG.Tables.schema(layer)) == "Tables.Schema:\n :FID        Float64\n :pointname  String\n Symbol(\"\")  wkbPoint::OGRwkbGeometryType = 0x00000001"
     @test sprint(print, AG.Base.iterate(AG.GeoTable(layer))) == "(ArchGDAL.FeatureRow(NamedTuple{(:pointname, :geometry, :FID),Tuple{String,ArchGDAL.IGeometry,Float64}}[(pointname = \"point-a\", geometry = Geometry: POINT (100 0), FID = 2.0)], 0), 1)"
     @test Tables.istable(AG.GeoTable(layer)) == true
     @test Tables.rowaccess(AG.GeoTable(layer)) == true
