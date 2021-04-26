3221556544fed87642cb5138afd3f019d787a44c
Yeesian Ng <ngyeesian@gmail.com>
2021-04-25 21:12:44 -0700

    Add test for copywholeraster!

--- test/test_array.jl
+++ test/test_array.jl
@@ -155,6 +155,7 @@ end
         AG.copy(ds) do copy1
             @test typeof(AG.copywholeraster!(ds, copy1)) == typeof(copy1)
             @test typeof(AG.copywholeraster!(AG.RasterDataset(copy1), ds)) == typeof(ds)
+            @test typeof(AG.copywholeraster!(copy1, ds)) == typeof(ds)
         end
     end
 end
