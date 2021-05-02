5ba0075d961d8a466b353b21d8eb5fb46803a09b
Yeesian Ng <ngyeesian@gmail.com>
2021-05-01 17:14:48 -0700

    Add tests for images
    Removed default support for HSL rasters since I haven't seen any examples in the wild of it actually working from GDAL, and we'll benefit from seeing actual files if someone actually wants to use it and runs into the lack of support for it.

--- src/raster/images.jl
+++ src/raster/images.jl
@@ -1,42 +1,40 @@
 function imread(
         colortable::ColorTable,
         dataset::AbstractDataset,
-        indices::NTuple{1, <:Integer}
+        indices
     )
-	palette = getcolortable(getband(dataset,indices[1])) do ct
-    	getpaletteinterp(ct)
+    palette = getcolortable(getband(dataset, indices[1])) do ct
+        paletteinterp(ct)
     end
-    colortype = if palette == GDAL.GPI_Gray;    ColorTypes.Gray
-    elseif palette == GDAL.GPI_RGB;             ColorTypes.RGB
-    elseif palette == GDAL.GPI_HLS;             ColorTypes.HSL
-    elseif palette == GDAL.GPI_CMYK
+    colortype = if palette == GPI_Gray
+        ColorTypes.Gray
+    elseif palette == GPI_RGB
+        ColorTypes.RGBA
+    else
         error("""
-        CMYK not yet supported. Please file an issue at
+        $palette not yet supported. Please file an issue at
         https://github.com/yeesian/ArchGDAL.jl/issues
         """)
-    else; error("Unknown color palette: $palette")
     end
-    return ImageCore.permuteddimsview(ImageCore.colorview(colortype,
-        ImageCore.normedview(read(dataset, indices[1]))
-    ), (2,1))
+    return imread(colortype, dataset, Tuple(indices))
 end
 
 function imread(
-        colortype::Type{ColorTypes.Gray},
+        colortype::Type{<:ColorTypes.Colorant},
         dataset::AbstractDataset,
         indices::NTuple{1, <:Integer}
     )
-    return ImageCore.permuteddimsview(ImageCore.colorview(ColorTypes.Gray,
+    return ImageCore.PermutedDimsArray(ImageCore.colorview(colortype,
         ImageCore.normedview(read(dataset, indices[1]))
     ), (2,1))
 end
 
 function imread(
-        colortype::Type{ColorTypes.RGB},
+        colortype::Type{<:ColorTypes.Colorant},
         dataset::AbstractDataset,
         indices::NTuple{3,<:Integer}
     )
-    return ImageCore.permuteddimsview(ImageCore.colorview(ColorTypes.RGB,
+    return ImageCore.PermutedDimsArray(ImageCore.colorview(colortype,
         ImageCore.normedview(read(dataset, indices[1])),
         ImageCore.normedview(read(dataset, indices[2])),
         ImageCore.normedview(read(dataset, indices[3]))
@@ -44,11 +42,11 @@ function imread(
 end
 
 function imread(
-        colortype::Type{ColorTypes.RGBA},
+        colortype::Type{<:ColorTypes.Colorant},
         dataset::Dataset,
         indices::NTuple{4,<:Integer}
     )
-    return ImageCore.permuteddimsview(ImageCore.colorview(ColorTypes.RGBA,
+    return ImageCore.PermutedDimsArray(ImageCore.colorview(colortype,
         ImageCore.normedview(read(dataset, indices[1])),
         ImageCore.normedview(read(dataset, indices[2])),
         ImageCore.normedview(read(dataset, indices[3])),
@@ -56,48 +54,34 @@ function imread(
     ), (2,1))
 end
 
-function imread(
-        colortype::Type{ColorTypes.YCbCr},
-        dataset::Dataset,
-        indices::NTuple{4,<:Integer}
-    )
-    return ImageCore.permuteddimsview(ImageCore.colorview(ColorTypes.YCbCr,
-        ImageCore.normedview(read(dataset, indices[1])),
-        ImageCore.normedview(read(dataset, indices[2])),
-        ImageCore.normedview(read(dataset, indices[3])),
-        ImageCore.normedview(read(dataset, indices[4]))
-    ), (2,1))
-end
-
-function imread(dataset::AbstractDataset, indices::Vector{<:Integer})
-    gci = GDALColorInterp[getcolorinterp(getband(dataset, i)) for i in indices]
+function imread(dataset::AbstractDataset, indices)
+    gci = unique(GDALColorInterp[
+        getcolorinterp(getband(dataset, i)) for i in indices
+    ])
     gciorder = sort(gci)
-    colortype = if gciorder == [GCI_GrayIndex]
-    	ColorTypes.Gray
+    return if gciorder == [GCI_GrayIndex]
+        imread(ColorTypes.Gray, dataset, Tuple(indices[sortperm(gci)]))
     elseif gciorder == [GCI_PaletteIndex]
-    	ColorTable
+        getcolortable(getband(dataset, 1)) do ct
+            imread(ct, dataset, indices)
+        end
     elseif gciorder == [GCI_RedBand, GCI_GreenBand, GCI_BlueBand]
-    	ColorTypes.RGB
+        imread(ColorTypes.RGB, dataset, Tuple(indices[sortperm(gci)]))
     elseif gciorder == [GCI_RedBand, GCI_GreenBand, GCI_BlueBand, GCI_AlphaBand]
-    	ColorTypes.RGBA
+        imread(ColorTypes.RGBA, dataset, Tuple(indices[sortperm(gci)]))
     elseif gciorder == [GCI_HueBand, GCI_SaturationBand, GCI_LightnessBand]
-    	ColorTypes.HSL
-	# elseif gciorder == [
-	# 		GCI_CyanBand, GCI_MagentaBand, GCI_YellowBand, GCI_BlackBand
-	# 	]
-    #   ColorTypes.CMYK
+        imread(ColorTypes.HSL, dataset, Tuple(indices[sortperm(gci)]))
     elseif gciorder == [GCI_YCbCr_YBand, GCI_YCbCr_CbBand, GCI_YCbCr_CrBand]
-        ColorTypes.YCbCr
+        imread(ColorTypes.YCbCr, dataset, Tuple(indices[sortperm(gci)]))
     else
         error("""
-        Unknown GCI: $colororder. Please file an issue at
+        Unknown GCI: $gciorder. Please file an issue at
         https://github.com/yeesian/ArchGDAL.jl/issues
         """)
     end
-    return imread(colortype, dataset, Tuple(indices[sortperm(gci)]))
 end
 
-imread(dataset::AbstractDataset) = imread(dataset, collect(1:nraster(dataset)))
+imread(dataset::AbstractDataset) = imread(dataset, 1:nraster(dataset))
 
 function imread(filename::String)
     return read(filename) do dataset
--- test/test_images.jl
+++ test/test_images.jl
@@ -1,11 +1,60 @@
 using Test
 import ArchGDAL; const AG = ArchGDAL
 import ImageCore
+import ColorTypes
+
+@testset "test_images.jl" begin
 
 @testset "Test Gray colors" begin
-	@test AG.imread("data/utmsmall.tif") isa ImageCore.PermutedDimsArray
+    @test eltype(AG.imread("data/utmsmall.tif")) ==
+        ColorTypes.Gray{ImageCore.N0f8}
 end
 
-@testset "Test RGBA colors" begin
-	@test AG.imread("gdalworkshop/world.tif") isa ImageCore.PermutedDimsArray
+AG.read("gdalworkshop/world.tif") do dataset
+    @testset "Test RGB colors" begin
+        @test eltype(AG.imread(dataset)) ==
+            ColorTypes.RGB{ImageCore.N0f8}
+    end
+end
+
+AG.create(
+        AG.getdriver("MEM"),
+        width = 2,
+        height = 2,
+        nbands = 4,
+        dtype = UInt8,
+    ) do dataset
+    @testset "Test RGBA colors" begin
+        AG.setcolorinterp!(AG.getband(dataset, 1), AG.GCI_RedBand)
+        AG.setcolorinterp!(AG.getband(dataset, 2), AG.GCI_GreenBand)
+        AG.setcolorinterp!(AG.getband(dataset, 3), AG.GCI_BlueBand)
+        AG.setcolorinterp!(AG.getband(dataset, 4), AG.GCI_AlphaBand)
+        @test eltype(AG.imread(dataset)) ==
+            ColorTypes.RGBA{ImageCore.N0f8}
+    end
+
+    @testset "Test ColorTable colors" begin
+        AG.setcolorinterp!(AG.getband(dataset, 1), AG.GCI_PaletteIndex)
+        AG.setcolorinterp!(AG.getband(dataset, 2), AG.GCI_PaletteIndex)
+        AG.setcolorinterp!(AG.getband(dataset, 3), AG.GCI_PaletteIndex)
+        AG.setcolorinterp!(AG.getband(dataset, 4), AG.GCI_PaletteIndex)
+        
+        AG.createcolortable(AG.GPI_RGB) do ct
+            AG.setcolortable!(AG.getband(dataset, 1), ct)
+            AG.setcolortable!(AG.getband(dataset, 2), ct)
+            AG.setcolortable!(AG.getband(dataset, 3), ct)
+            AG.setcolortable!(AG.getband(dataset, 4), ct)
+        end
+        @test eltype(AG.imread(dataset)) ==
+            ColorTypes.RGBA{ImageCore.N0f8}
+
+        AG.createcolortable(AG.GPI_CMYK) do ct # CMYK not supported yet
+            AG.setcolortable!(AG.getband(dataset, 1), ct)
+            AG.setcolortable!(AG.getband(dataset, 2), ct)
+            AG.setcolortable!(AG.getband(dataset, 3), ct)
+        end
+        @test_throws ErrorException AG.imread(dataset, 1:3)
+    end
+end
+
 end
