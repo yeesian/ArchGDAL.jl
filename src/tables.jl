yeesian/ArchGDAL.jl

commit c1b4cf0bb9ac94e531af9da7a40fffac70cb710c

Give geometry type hints in Schema. (#160)
* Give geometry type hints in Schema.

* Also change IGeometry type.

* Also converted geometry. Optimized getgeomtype call. Added several tests.

* Merged master and fixed display test.

* geometry first, fields second

Co-authored-by: Martijn Visser <mgvisser@gmail.com>

src/tables.jl
@@ -13,10 +13,9 @@ getlayer(t::Table) = Base.getfield(t, :layer)
 function Tables.schema(layer::AbstractFeatureLayer)
     field_names, geom_names, featuredefn, fielddefns = schema_names(layer)
     ngeom = ArchGDAL.ngeom(featuredefn)
-    geomdefns = (ArchGDAL.getgeomdefn(featuredefn, i) for i in 0:ngeom-1)
+    geomtypes = (IGeometry{ArchGDAL.gettype(ArchGDAL.getgeomdefn(featuredefn, i))} for i in 0:ngeom-1)
     field_types = (_FIELDTYPE[gettype(fielddefn)] for fielddefn in fielddefns)
-    geom_types = (IGeometry for i in 1:ngeom)
-    Tables.Schema((field_names..., geom_names...), (field_types..., geom_types...))
+    Tables.Schema((geom_names..., field_names...), (geomtypes..., field_types...))
 end
 
 Tables.istable(::Type{<:Table}) = true
@@ -34,7 +33,7 @@ end
 
 function Base.getindex(t::Table, idx::Integer)
     layer = getlayer(t)
-    setnextbyindex!(layer, idx-1) 
+    setnextbyindex!(layer, idx-1)
     return nextnamedtuple(layer)
 end
 
@@ -55,7 +54,7 @@ function nextnamedtuple(layer::IFeatureLayer)
     return nextfeature(layer) do feature
         prop = (getfield(feature, name) for name in field_names)
         geom = (getgeom(feature, idx-1) for idx in 1:length(geom_names))
-        NamedTuple{(field_names..., geom_names...)}((prop..., geom...))
+        NamedTuple{(geom_names..., field_names...)}((geom..., prop...))
     end
 end
 
@@ -63,7 +62,8 @@ function schema_names(layer::AbstractFeatureLayer)
     featuredefn = layerdefn(layer)
     fielddefns = (getfielddefn(featuredefn, i) for i in 0:nfield(layer)-1)
     field_names = (Symbol(getname(fielddefn)) for fielddefn in fielddefns)
-    geom_names = (Symbol(getname(getgeomdefn(featuredefn, i-1))) for i in 1:ngeom(layer))
+    geom_names = collect(Symbol(getname(getgeomdefn(featuredefn, i-1))) for i in 1:ngeom(layer))
+    replace!(geom_names, Symbol("")=>Symbol("geometry"), count=1)
     return (field_names, geom_names, featuredefn, fielddefns)
 end
 

test/test_tables.jl
@@ -44,9 +44,9 @@ using Tables
         @test Base.length(gt2) == 9
         @test Base.IteratorSize(typeof(gt)) == Base.HasLength()
         @test Base.IteratorEltype(typeof(gt1)) == Base.HasEltype()
-        @test propertynames(gt) == (:FID, :pointname, Symbol(""))
-        @test propertynames(gt1) == (:id, :zoom, :location, :point, :linestring)
-        @test propertynames(gt2) == (:id, :zoom, :location, :point, :linestring)
+        @test propertynames(gt) == (:geometry, :FID, :pointname)
+        @test propertynames(gt1) == (:point, :linestring, :id, :zoom, :location)
+        @test propertynames(gt2) == (:point, :linestring, :id, :zoom, :location)
         @test getproperty(gt, :FID) == [iterate(gt, i)[1].FID for i in 0:size(gt)-1]
         @test getproperty(gt1, :zoom) == [iterate(gt1, i)[1].zoom for i in 0:size(gt1)-1]
         @test sprint(print, gt2[5].linestring) == sprint(print, gt2[3].point)
@@ -59,10 +59,10 @@ using Tables
 
         AG.resetreading!(layer)
         AG.resetreading!(layer1)
-        
-        @test AG.nextnamedtuple(layer) isa NamedTuple{(:FID, :pointname, Symbol("")),Tuple{Float64,String,ArchGDAL.IGeometry}}
-        @test AG.nextnamedtuple(layer1) isa NamedTuple{(:id, :zoom, :location, :point, :linestring),Tuple{String,String,String,ArchGDAL.IGeometry,ArchGDAL.IGeometry}}
-        for i in 1:4
+
+        @test AG.nextnamedtuple(layer) isa NamedTuple{(:geometry, :FID, :pointname),Tuple{ArchGDAL.IGeometry{AG.GDAL.wkbPoint},Float64,String}}
+        @test AG.nextnamedtuple(layer1) isa NamedTuple{(:point, :linestring, :id, :zoom, :location),Tuple{ArchGDAL.IGeometry{AG.GDAL.wkbPoint},ArchGDAL.IGeometry{AG.GDAL.wkbLineString},String,String,String}}
+        for i in (1,3,4)
             @test AG.schema_names(layer)[i] isa Base.Generator || AG.schema_names(layer)[i] isa ArchGDAL.IFeatureDefnView
             @test AG.schema_names(layer1)[i] isa Base.Generator || AG.schema_names(layer1)[i] isa ArchGDAL.IFeatureDefnView
         end

src/ogr/geometry.jl
@@ -298,7 +298,7 @@ end
 
 Fetch geometry type code
 """
-getgeomtype(geom::AbstractGeometry) = GDAL.ogr_g_getgeometrytype(geom.ptr)
+getgeomtype(geom::AbstractGeometry) = _geomtype(geom)
 
 """
     geomname(geom::AbstractGeometry)

src/types.jl
@@ -205,21 +205,23 @@ mutable struct ISpatialRef <: AbstractSpatialRef
     end
 end
 
-mutable struct Geometry <: AbstractGeometry
+mutable struct Geometry{OGRwkbGeometryType} <: AbstractGeometry
     ptr::GDALGeometry
 
-    Geometry(ptr::GDALGeometry = C_NULL) = new(ptr)
+    Geometry(ptr::GDALGeometry = C_NULL) = new{ptr != C_NULL ? GDAL.ogr_g_getgeometrytype(ptr) : GDAL.wkbUnknown}(ptr)
 end
+_geomtype(::Geometry{T}) where T = T
 
-mutable struct IGeometry <: AbstractGeometry
+mutable struct IGeometry{OGRwkbGeometryType} <: AbstractGeometry
     ptr::GDALGeometry
 
     function IGeometry(ptr::GDALGeometry = C_NULL)
-        geom = new(ptr)
+        geom = new{ptr != C_NULL ? GDAL.ogr_g_getgeometrytype(ptr) : GDAL.wkbUnknown}(ptr)
         finalizer(destroy, geom)
         return geom
     end
 end
+_geomtype(::IGeometry{T}) where T = T
 
 mutable struct ColorTable
     ptr::GDALColorTable

test/test_display.jl
@@ -20,7 +20,7 @@ import ArchGDAL; const AG = ArchGDAL
              Field 1 (pointname): [OFTString], point-a, point-b, a, b
         """
         @test sprint(print, AG.nextnamedtuple(layer), context=:compact => true) == 
-        "(FID = 2.0, pointname = \"point-a\",  = Geometry: wkbPoint)"
+        "(geometry = Geometry: wkbPoint, FID = 2.0, pointname = \"point-a\")"
         @test sprint(print, AG.layerdefn(layer)) == """
           Geometry (index 0):  (wkbPoint)
              Field (index 0): FID (OFTReal)

test/test_geometry.jl
@@ -26,6 +26,7 @@ end
         @test AG.issimple(point) == true
         @test AG.isring(point) == false
         @test AG.getz(point, 0) == 0
+        @test typeof(point) == AG.Geometry{GDAL.wkbPoint}
 
         @test sprint(print, AG.envelope(point)) == "GDAL.OGREnvelope(100.0, 100.0, 70.0, 70.0)"
         @test sprint(print, AG.envelope3d(point)) == "GDAL.OGREnvelope3D(100.0, 100.0, 70.0, 70.0, 0.0, 0.0)"
@@ -107,7 +108,8 @@ end
         @test AG.toWKT(geom) == "LINESTRING (1 4,2 5,3 6)"
         AG.setpoint!(geom, 1, 10, 10)
         @test AG.toWKT(geom) == "LINESTRING (1 4,10 10,3 6)"
-        @test GFT.val(convert(GFT.WellKnownText, geom)) == AG.toWKT(geom)  
+        @test GFT.val(convert(GFT.WellKnownText, geom)) == AG.toWKT(geom)
+        @test typeof(geom) == AG.Geometry{GDAL.wkbLineString}
     end
     AG.createlinestring([1.,2.,3.], [4.,5.,6.], [7.,8.,9.]) do geom
         @test AG.toWKT(geom) == "LINESTRING (1 4 7,2 5 8,3 6 9)"
@@ -116,7 +118,7 @@ end
         AG.addpoint!(geom, 11, 11, 11)
         @test AG.toWKT(geom) == "LINESTRING (1 4 7,10 10 10,3 6 9,11 11 11)"
     end
-    
+
     @test AG.toWKT(AG.createlinearring([1.,2.,3.], [4.,5.,6.])) == "LINEARRING (1 4,2 5,3 6)"
     AG.createlinearring([1.,2.,3.], [4.,5.,6.]) do geom
         # @test GeoInterface.geotype(geom) == :LinearRing
@@ -126,6 +128,7 @@ end
         @test AG.toWKT(geom) == "LINEARRING (1 4,2 5,3 6,0 0,0 0)"
         AG.empty!(geom)
         @test AG.toWKT(geom) == "LINEARRING EMPTY"
+        @test typeof(geom) == AG.Geometry{GDAL.wkbLineString}  # this seems odd
     end
     AG.createlinearring([1.,2.,3.], [4.,5.,6.], [7.,8.,9.]) do geom
         @test AG.toWKT(geom) == "LINEARRING (1 4 7,2 5 8,3 6 9)"
@@ -138,6 +141,7 @@ end
         @test GeoInterface.geotype(geom) == :Polygon
         @test isapprox(GeoInterface.coordinates(geom), [[[1,4],[2,5],[3,6]]], atol=1e-6)
         @test AG.toWKT(geom) == "POLYGON ((1 4,2 5,3 6))"
+        @test typeof(geom) == AG.Geometry{GDAL.wkbPolygon}
     end
     AG.createpolygon([1.,2.,3.], [4.,5.,6.], [7.,8.,9.]) do geom
         @test AG.toWKT(geom) == "POLYGON ((1 4 7,2 5 8,3 6 9))"
@@ -150,6 +154,7 @@ end
         @test GeoInterface.geotype(geom) == :MultiPoint
         @test isapprox(GeoInterface.coordinates(geom), [[1,4],[2,5],[3,6]], atol=1e-6)
         @test AG.toWKT(geom) == "MULTIPOINT (1 4,2 5,3 6)"
+        @test typeof(geom) == AG.Geometry{GDAL.wkbMultiPoint}
     end
     AG.createmultipoint([1.,2.,3.], [4.,5.,6.], [7.,8.,9.]) do geom
         @test AG.toWKT(geom) == "MULTIPOINT (1 4 7,2 5 8,3 6 9)"
@@ -179,18 +184,24 @@ end
             atol=1e-6
         )
         @test AG.toWKT(geom) == "MULTIPOLYGON (((0 0,0 4,4 4,4 0),(1 1,1 3,3 3,3 1)),((10 0,10 4,14 4,14 0),(11 1,11 3,13 3,13 1)))"
+        @test typeof(geom) == AG.Geometry{GDAL.wkbMultiPolygon}
     end
 
     AG.fromWKT("CURVEPOLYGON(CIRCULARSTRING(-2 0,-1 -1,0 0,1 -1,2 0,0 2,-2 0),(-1 0,0 0.5,1 0,0 1,-1 0))") do geom
+        @test typeof(geom) == AG.Geometry{GDAL.wkbCurvePolygon}
         @test AG.toWKT(AG.curvegeom(AG.lineargeom(geom, 0.5))) == "CURVEPOLYGON (CIRCULARSTRING (-2 0,-1 -1,0 0,1 -1,2 0,0 2,-2 0),(-1 0,0.0 0.5,1 0,0 1,-1 0))"
         AG.lineargeom(geom, 0.5) do lgeom
+            @test typeof(lgeom) == AG.Geometry{GDAL.wkbPolygon}
             AG.curvegeom(lgeom) do clgeom
                 @test AG.toWKT(clgeom) == "CURVEPOLYGON (CIRCULARSTRING (-2 0,-1 -1,0 0,1 -1,2 0,0 2,-2 0),(-1 0,0.0 0.5,1 0,0 1,-1 0))"
+                @test typeof(clgeom) == AG.Geometry{GDAL.wkbCurvePolygon}
             end
             @test AG.ngeom(AG.polygonize(AG.forceto(lgeom, GDAL.wkbMultiLineString))) == 2
             AG.forceto(lgeom, GDAL.wkbMultiLineString) do mlsgeom
+                @test typeof(mlsgeom) == AG.Geometry{GDAL.wkbMultiLineString}
                 AG.polygonize(mlsgeom) do plgeom
                     @test AG.ngeom(plgeom) == 2
+                    @test typeof(plgeom) == AG.Geometry{GDAL.wkbGeometryCollection}
                 end
             end
         end
@@ -232,6 +243,7 @@ end
         @test AG.toWKT(result) == "MULTIPOLYGON (((0 4 8,4 4 8,4 0 8,0 0 8,0 4 8),(3 1 8,3 3 8,1 3 8,1 1 8,3 1 8)),((10 4 8,14 4 8,14 0 8,10 0 8,10 4 8),(13 1 8,13 3 8,11 3 8,11 1 8,13 1 8)))"
         AG.segmentize!(result, 2)
         @test AG.toWKT(result) == "MULTIPOLYGON (((0 4 8,2 4 8,4 4 8,4 2 8,4 0 8,2 0 8,0 0 8,0 2 8,0 4 8),(3 1 8,3 3 8,1 3 8,1 1 8,3 1 8)),((10 4 8,12 4 8,14 4 8,14 2 8,14 0 8,12 0 8,10 0 8,10 2 8,10 4 8),(13 1 8,13 3 8,11 3 8,11 1 8,13 1 8)))"
+        @test typeof(result) == AG.Geometry{GDAL.wkbMultiPolygon25D}
     end
 
     @test AG.toWKT(AG.symdifference(geom1, geom2)) == "GEOMETRYCOLLECTION (POLYGON ((0 4 8,4 4 8,4 0 8,0 0 8,0 4 8),(3 1 8,3 3 8,1 3 8,1 1 8,3 1 8)),POLYGON ((10 4 8,14 4 8,14 0 8,10 0 8,10 4 8),(13 1 8,13 3 8,11 3 8,11 1 8,13 1 8)),POINT (2 5 8),POINT (3 6 9))"
@@ -242,12 +254,14 @@ end
         @test AG.toWKT(result) == "GEOMETRYCOLLECTION (POLYGON ((0 4 8,4 4 8,4 0 8,0 0 8,0 4 8),(3 1 8,3 3 8,1 3 8,1 1 8,3 1 8)),POINT (2 5 8),POINT (3 6 9))"
         AG.removeallgeoms!(result)
         @test AG.toWKT(result) == "GEOMETRYCOLLECTION EMPTY"
+        @test typeof(result) == AG.Geometry{GDAL.wkbGeometryCollection25D}
     end
 
     geom3 = AG.fromWKT("GEOMETRYCOLLECTION (POINT (2 5 8),POLYGON ((0 0 8,0 4 8,4 4 8,4 0 8,0 0 8),(1 1 8,3 1 8,3 3 8,1 3 8,1 1 8)),POLYGON ((10 0 8,10 4 8,14 4 8,14 0 8,10 0 8),(11 1 8,13 1 8,13 3 8,11 3 8,11 1 8)), POINT EMPTY)")
     AG.clone(geom3) do geom4
         @test sprint(print, AG.clone(geom3)) == "Geometry: GEOMETRYCOLLECTION (POINT (2 5 8),POLYGON ((0 0 8, ... MPTY)"
         @test sprint(print, AG.clone(geom4)) == "Geometry: GEOMETRYCOLLECTION (POINT (2 5 8),POLYGON ((0 0 8, ... MPTY)"
+        @test typeof(geom4) == AG.Geometry{GDAL.wkbGeometryCollection25D}
     end
     AG.clone(AG.getgeom(geom3, 3)) do geom4
         @test sprint(print, geom4) == "Geometry: POINT EMPTY"

commit d2ae1bd1bff8a2378e871ca681deb3aed8031122

Undo replacement of empty geometry column names with "geometry".
If we just do the replacement in the schema, the schema will be out of sync with the geometry column names of the individual features in the layer: this causes issues that were not detected when it was first introduced in #160.

src/tables.jl
@@ -55,6 +55,5 @@ function schema_names(featuredefn::IFeatureDefnView)
         Symbol(getname(getgeomdefn(featuredefn, i - 1))) for
         i in 1:ngeom(featuredefn)
     )
-    replace!(geom_names, Symbol("") => Symbol("geometry"), count = 1)
     return (geom_names, field_names, featuredefn, fielddefns)
 end

test/test_tables.jl
@@ -83,7 +83,7 @@ using Tables
             @test isnothing(Tables.getcolumn(features[2], :fake))
 
             geom_names, field_names = AG.schema_names(AG.layerdefn(layer))
-            @test collect(geom_names) == [:geometry]
+            @test collect(geom_names) == [Symbol("")]
             @test collect(field_names) == [:FID, :pointname]
             geom_names, field_names = AG.schema_names(AG.layerdefn(layer1))
             @test collect(geom_names) == [:point, :linestring]