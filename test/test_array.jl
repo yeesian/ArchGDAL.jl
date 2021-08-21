d0e7b57b1ae12da3fb14861af29d0d61ae432b6a
yeesian <yeesian@users.noreply.github.com>
2021-08-21 00:05:19 +0000

    Format .jl files

--- test/test_array.jl
+++ test/test_array.jl
@@ -171,7 +171,9 @@ const AG = ArchGDAL;
             nbands = 0,
             dtype = UInt8,
         ) do dataset
-            @test_throws Union{ArgumentError,MethodError} AG.RasterDataset(dataset)
+            @test_throws Union{ArgumentError,MethodError} AG.RasterDataset(
+                dataset,
+            )
             @test_throws DimensionMismatch AG._common_size(dataset)
         end
     end
