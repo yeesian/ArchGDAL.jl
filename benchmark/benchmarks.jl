173c468a255f536edf7362a6514aa079204ff262
mathieu17g <mathieu.biskupski@icloud.com>
2021-10-05 19:55:45 +0200

    benchmark/data directory cleanup and JuliaFormatter

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -7,38 +7,42 @@ using Shapefile
 
 const SUITE = BenchmarkGroup()
 
-SUITE["shapefile_to_table"] = BenchmarkGroup() 
+SUITE["shapefile_to_table"] = BenchmarkGroup()
 
 # Data preparation
+datadir = joinpath(@__DIR__, "data")
+!isdir(datadir) && mkdir(datadir)
+
 road_shapefile_ziparchive = joinpath(@__DIR__, "data/road.zip")
 !isfile(road_shapefile_ziparchive) && download(
     "https://www.data.gouv.fr/fr/datasets/r/ea2b85d7-b29e-4bb4-baa5-ac3bb618c67e",
     road_shapefile_ziparchive,
 )
+
 road_shapefile_dir = splitext(road_shapefile_ziparchive)[1]
 !isdir(road_shapefile_dir) && mkdir(road_shapefile_dir)
+
 ziparchive = ZipFile.Reader(road_shapefile_ziparchive)
 for file in ziparchive.files
     extracted_file = joinpath(road_shapefile_dir, file.name)
     !isfile(extracted_file) && write(extracted_file, read(file))
 end
 close(ziparchive)
-road_shapefile_file = joinpath(road_shapefile_dir, splitpath(road_shapefile_dir)[end] * ".shp")
+
+road_shapefile_file =
+    joinpath(road_shapefile_dir, splitpath(road_shapefile_dir)[end] * ".shp")
 
 # Benchmarks
-SUITE["shapefile_to_table"]["frenchroads_with_GDAL.jl_via_vsizip"] = @benchmarkable Tables.columns(
-    AG.getlayer(
-        AG.read("/vsizip/" * relpath($road_shapefile_ziparchive)), # relpath is a workaround in case there are spaces in local fullpath (incompatible with /vsizip usage) when benchmarkpkg is run locally
-        0,
-    ),
-)
-SUITE["shapefile_to_table"]["frenchroads_with_GDAL.jl"] = @benchmarkable Tables.columns(
-    AG.getlayer(
-        AG.read($road_shapefile_file),
-        0,
-    ),
-)
-SUITE["shapefile_to_table"]["frenchroads_with_Shapefile.jl"] = @benchmarkable begin
-    Tables.columns(Shapefile.Table($road_shapefile_file))
-end
-
+SUITE["shapefile_to_table"]["frenchroads_with_GDAL.jl_via_vsizip"] =
+    @benchmarkable Tables.columns(
+        AG.getlayer(
+            AG.read("/vsizip/" * relpath($road_shapefile_ziparchive)), # relpath is a workaround in case there are spaces in local fullpath (incompatible with /vsizip usage) when benchmarkpkg is run locally
+            0,
+        ),
+    )
+SUITE["shapefile_to_table"]["frenchroads_with_GDAL.jl"] =
+    @benchmarkable Tables.columns(AG.getlayer(AG.read($road_shapefile_file), 0))
+SUITE["shapefile_to_table"]["frenchroads_with_Shapefile.jl"] =
+    @benchmarkable begin
+        Tables.columns(Shapefile.Table($road_shapefile_file))
+    end
