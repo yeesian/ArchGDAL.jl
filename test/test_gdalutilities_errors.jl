1f96ef7e4f943802daa207b16fc1e7ef3936442e
mathieu17g <mathieu.biskupski@icloud.com>
2021-10-09 11:25:08 +0200

    Tried to test errored  `AG.galinfo`  with `@test` instead of `@test_throws`

--- test/test_gdalutilities_errors.jl
+++ test/test_gdalutilities_errors.jl
@@ -5,10 +5,21 @@ using Test
 @testset "test_gdalutilities_errors.jl" begin
     AG.read("data/utmsmall.tif") do ds_small
         @testset "GDAL Error" begin
-            @test_throws GDAL.GDALError AG.gdalinfo(
-                ds_small,
-                ["-novalidoption"],
-            )
+            # @test_throws GDAL.GDALError AG.gdalinfo(
+            #     ds_small,
+            #     ["-novalidoption"],
+            # )
+            @test begin 
+                "GDAL.GDALError(GDAL.CE_Failure, 6, \"Unknown option name '-novalidoption'\")" == 
+                    try
+                        AG.gdalinfo(
+                            ds_small,
+                            ["-novalidoption"],
+                        )
+                    catch e
+                        string(e)
+                    end
+            end
             # @test_throws GDAL.GDALError AG.unsafe_gdaltranslate(
             #     ds_small,
             #     ["-novalidoption"],
