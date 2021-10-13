6454e0973a7508d5c94cb1d40d82942e9f2d740f
mathieu17g <72861595+mathieu17g@users.noreply.github.com>
2021-10-13 07:03:48 +0200

    Fixed arg type in `ctv_toWKT` signature ("Table to layer conversion" testset)

--- test/test_tables.jl
+++ test/test_tables.jl
@@ -802,8 +802,8 @@ using Tables
             toWKT_withmissings(x::AG.AbstractGeometry) = AG.toWKT(x)
             toWKT_withmissings(x::Any) = x
 
-            function ctv_toWKT(x::Tables.ColumnTable)
-                return Tuple(toWKT_withmissings.(x[i]) for i in 1:length(x))
+            function ctv_toWKT(x::T) where {T <: NTuple{N, AbstractArray{S, D} where S}} where {N, D}
+                return Tuple(toWKT_withmissings.(x[i]) for i in 1:N)
             end
 
             """
