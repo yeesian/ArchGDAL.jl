d28f70cb0fce6eadc28ab579fab75037db0e0de0
mathieu17g <72861595+mathieu17g@users.noreply.github.com>
2022-01-14 18:26:05 +0100

    Added new Tables.jl interface to BenchmarkCI

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -25,15 +25,18 @@ road_shapefile_file =
     joinpath(road_shapefile_dir, splitpath(road_shapefile_dir)[end] * ".shp")
 
 # Benchmarks
-SUITE["shapefile_to_table"]["frenchroads_with_GDAL.jl_via_vsizip"] =
+SUITE["shapefile_to_table"]["frenchroads_with_ArchGDAL.jl_via_vsizip"] =
     @benchmarkable Tables.columns(
-        AG.getlayer(
-            AG.read("/vsizip/" * relpath($road_shapefile_ziparchive)), # relpath is a workaround in case there are spaces in local fullpath (incompatible with /vsizip usage) when benchmarkpkg is run locally
-            0,
-        ),
+        AG.getlayer(AG.read("/vsizip/" * relpath($road_shapefile_ziparchive))), # relpath is a workaround in case there are spaces in local fullpath (incompatible with /vsizip usage) when benchmarkpkg is run locally
     )
-SUITE["shapefile_to_table"]["frenchroads_with_GDAL.jl"] =
+SUITE["shapefile_to_table"]["frenchroads_with_ArchGDAL.jl"] =
     @benchmarkable Tables.columns(AG.getlayer(AG.read($road_shapefile_file), 0))
+SUITE["shapefile_to_table"]["frenchroads_with_ArchGDAL.jl_new_Tables.jl_interface_via_vsizip"] =
+    @benchmarkable Tables.columns(
+        AG.Table(AG.read("/vsizip/" * relpath($road_shapefile_ziparchive))), # relpath is a workaround in case there are spaces in local fullpath (incompatible with /vsizip usage) when benchmarkpkg is run locally
+    )
+SUITE["shapefile_to_table"]["frenchroads_with_ArchGDAL.jl_new_Tables.jl_interface"] =
+    @benchmarkable Tables.columns(AG.Table($road_shapefile_file))
 SUITE["shapefile_to_table"]["frenchroads_with_Shapefile.jl"] =
     @benchmarkable begin
         Tables.columns(Shapefile.Table($road_shapefile_file))
