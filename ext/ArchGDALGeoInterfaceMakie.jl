e0b43a571e8701979cad0709043bbcc1259a268a
Felix Cremer <fcremer@bgc-jena.mpg.de>
2023-12-06 14:50:20 +0100

    Remove print statement

--- ext/ArchGDALGeoInterfaceMakie.jl
+++ ext/ArchGDALGeoInterfaceMakie.jl
@@ -2,7 +2,6 @@ module ArchGDALGeoInterfaceMakie
 using GeoInterfaceMakie: GeoInterfaceMakie
 using ArchGDAL: ArchGDAL
 
-println("ArchGDALGeoInterfaceMakie.jl")
 GeoInterfaceMakie.@enable ArchGDAL.IGeometry
 GeoInterfaceMakie.@enable ArchGDAL.Geometry
 end
