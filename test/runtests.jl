b85b5d558d42e0ad4d93cbd5f5af935a9fb185a0
Erik Schnetter <schnetter@gmail.com>
2024-07-24 12:38:12 -0400

    Re-enable tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,48 +7,48 @@ import Aqua
 
 include("test_mdarray.jl")
 
-#TODO # ensure all testing files are present
-#TODO include("remotefiles.jl")
-#TODO 
-#TODO @testset "ArchGDAL" begin
-#TODO     cd(dirname(@__FILE__)) do
-#TODO         isdir("tmp") || mkpath("tmp")
-#TODO         #TODO include("test_doctest.jl")
-#TODO         #TODO include("test_convert.jl")
-#TODO         #TODO include("test_tables.jl")
-#TODO         #TODO include("test_gdal_tutorials.jl")
-#TODO         #TODO include("test_geometry.jl")
-#TODO         #TODO include("test_types.jl")
-#TODO         #TODO include("test_display.jl")
-#TODO         #TODO include("test_drivers.jl")
-#TODO         #TODO include("test_feature.jl")
-#TODO         #TODO include("test_featurelayer.jl")
-#TODO         #TODO include("test_fielddefn.jl")
-#TODO         #TODO include("test_iterators.jl")
-#TODO         #TODO include("test_styletable.jl")
-#TODO         #TODO include("test_dataset.jl")
-#TODO         #TODO include("test_rasterband.jl")
-#TODO         #TODO include("test_rasterio.jl")
-#TODO         #TODO include("test_array.jl")
-#TODO         #TODO include("test_spatialref.jl")
-#TODO         #TODO include("test_gdalutilities.jl")
-#TODO         #TODO include("test_gdalutilities_errors.jl")
-#TODO         #TODO include("test_rasterattrtable.jl")
-#TODO         include("test_mdarray.jl")
-#TODO         #TODO include("test_ospy_examples.jl")
-#TODO         #TODO include("test_geos_operations.jl")
-#TODO         #TODO include("test_cookbook_geometry.jl")
-#TODO         #TODO include("test_cookbook_projection.jl")
-#TODO         #TODO include("test_geotransform.jl")
-#TODO         #TODO include("test_images.jl")
-#TODO         #TODO include("test_utils.jl")
-#TODO         #TODO include("test_prepared_geometry.jl")
-#TODO         #TODO Aqua.test_all(
-#TODO         #TODO     ArchGDAL;
-#TODO         #TODO     ambiguities = false,
-#TODO         #TODO     stale_deps = false,
-#TODO         #TODO     piracies = false,
-#TODO         #TODO )
-#TODO         return nothing
-#TODO     end
-#TODO end
+# ensure all testing files are present
+include("remotefiles.jl")
+
+@testset "ArchGDAL" begin
+    cd(dirname(@__FILE__)) do
+        isdir("tmp") || mkpath("tmp")
+        #TODO include("test_doctest.jl")
+        #TODO include("test_convert.jl")
+        #TODO include("test_tables.jl")
+        #TODO include("test_gdal_tutorials.jl")
+        #TODO include("test_geometry.jl")
+        #TODO include("test_types.jl")
+        #TODO include("test_display.jl")
+        #TODO include("test_drivers.jl")
+        #TODO include("test_feature.jl")
+        #TODO include("test_featurelayer.jl")
+        #TODO include("test_fielddefn.jl")
+        #TODO include("test_iterators.jl")
+        #TODO include("test_styletable.jl")
+        #TODO include("test_dataset.jl")
+        #TODO include("test_rasterband.jl")
+        #TODO include("test_rasterio.jl")
+        #TODO include("test_array.jl")
+        #TODO include("test_spatialref.jl")
+        #TODO include("test_gdalutilities.jl")
+        #TODO include("test_gdalutilities_errors.jl")
+        #TODO include("test_rasterattrtable.jl")
+        include("test_mdarray.jl")
+        #TODO include("test_ospy_examples.jl")
+        #TODO include("test_geos_operations.jl")
+        #TODO include("test_cookbook_geometry.jl")
+        #TODO include("test_cookbook_projection.jl")
+        #TODO include("test_geotransform.jl")
+        #TODO include("test_images.jl")
+        #TODO include("test_utils.jl")
+        #TODO include("test_prepared_geometry.jl")
+        #TODO Aqua.test_all(
+        #TODO     ArchGDAL;
+        #TODO     ambiguities = false,
+        #TODO     stale_deps = false,
+        #TODO     piracies = false,
+        #TODO )
+        return nothing
+    end
+end
