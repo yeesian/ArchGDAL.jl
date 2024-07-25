42685aa134beeb7a0ec18ade7267f3a7ef7a70cc
Erik Schnetter <schnetter@gmail.com>
2024-07-25 18:41:40 -0400

    Restore all test cases

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,42 +11,42 @@ include("remotefiles.jl")
 @testset "ArchGDAL" begin
     cd(dirname(@__FILE__)) do
         isdir("tmp") || mkpath("tmp")
-        #TODO include("test_doctest.jl")
-        #TODO include("test_convert.jl")
-        #TODO include("test_tables.jl")
-        #TODO include("test_gdal_tutorials.jl")
-        #TODO include("test_geometry.jl")
-        #TODO include("test_types.jl")
-        #TODO include("test_display.jl")
-        #TODO include("test_drivers.jl")
-        #TODO include("test_feature.jl")
-        #TODO include("test_featurelayer.jl")
-        #TODO include("test_fielddefn.jl")
-        #TODO include("test_iterators.jl")
-        #TODO include("test_styletable.jl")
-        #TODO include("test_dataset.jl")
-        #TODO include("test_rasterband.jl")
-        #TODO include("test_rasterio.jl")
-        #TODO include("test_array.jl")
-        #TODO include("test_spatialref.jl")
-        #TODO include("test_gdalutilities.jl")
-        #TODO include("test_gdalutilities_errors.jl")
-        #TODO include("test_rasterattrtable.jl")
+        include("test_doctest.jl")
+        include("test_convert.jl")
+        include("test_tables.jl")
+        include("test_gdal_tutorials.jl")
+        include("test_geometry.jl")
+        include("test_types.jl")
+        include("test_display.jl")
+        include("test_drivers.jl")
+        include("test_feature.jl")
+        include("test_featurelayer.jl")
+        include("test_fielddefn.jl")
+        include("test_iterators.jl")
+        include("test_styletable.jl")
+        include("test_dataset.jl")
+        include("test_rasterband.jl")
+        include("test_rasterio.jl")
+        include("test_array.jl")
+        include("test_spatialref.jl")
+        include("test_gdalutilities.jl")
+        include("test_gdalutilities_errors.jl")
+        include("test_rasterattrtable.jl")
         include("test_mdarray.jl")
-        #TODO include("test_ospy_examples.jl")
-        #TODO include("test_geos_operations.jl")
-        #TODO include("test_cookbook_geometry.jl")
-        #TODO include("test_cookbook_projection.jl")
-        #TODO include("test_geotransform.jl")
-        #TODO include("test_images.jl")
-        #TODO include("test_utils.jl")
-        #TODO include("test_prepared_geometry.jl")
-        #TODO Aqua.test_all(
-        #TODO     ArchGDAL;
-        #TODO     ambiguities = false,
-        #TODO     stale_deps = false,
-        #TODO     piracies = false,
-        #TODO )
+        include("test_ospy_examples.jl")
+        include("test_geos_operations.jl")
+        include("test_cookbook_geometry.jl")
+        include("test_cookbook_projection.jl")
+        include("test_geotransform.jl")
+        include("test_images.jl")
+        include("test_utils.jl")
+        include("test_prepared_geometry.jl")
+        Aqua.test_all(
+            ArchGDAL;
+            ambiguities = false,
+            stale_deps = false,
+            piracies = false,
+        )
         return nothing
     end
 end
