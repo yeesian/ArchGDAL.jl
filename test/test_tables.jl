2195837aa359d9b2c29d8a9760138e4563bb2769
yeesian <1231079+yeesian@users.noreply.github.com>
2025-04-06 00:16:56 +0000

    Format .jl files

--- test/test_tables.jl
+++ test/test_tables.jl
@@ -101,6 +101,7 @@ using Tables
             function clean_test_dataset_files()
                 for (drvshortname, file_extension) in
                     TEST_DS_DRIVERS_FILE_EXTENSIONS
+
                     isfile(
                         joinpath(
                             @__DIR__,
@@ -418,7 +419,7 @@ using Tables
             tupleoftuples_equal = (
                 (x, y) ->
                     length(x) == length(y) &&
-                        all([all(x[i] .=== y[i]) for i in 1:length(x)])
+                    all([all(x[i] .=== y[i]) for i in 1:length(x)])
             )
 
             """
